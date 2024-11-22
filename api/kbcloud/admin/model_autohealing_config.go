b449fbfc707cb89d02da320b2f532faddf7dfd36
wangyelei <wangyelei@users.noreply.github.com>
2024-11-22 08:51:28 +0000

    chore: auto update clinent api https://github.com/apecloud/apecloud/pull/7883

--- api/kbcloud/admin/model_autohealing_config.go
+++ api/kbcloud/admin/model_autohealing_config.go
@@ -10,14 +10,18 @@ import (
 
 // AutohealingConfig cluster instance autohealing process config
 type AutohealingConfig struct {
-	// Set to true to pause the auto-healing process, preventing automatic rebuilding of instances when a node fails or is in maintenance mode.
-	PauseAutoHealingOnNodeUnavailable bool `json:"pauseAutoHealingOnNodeUnavailable"`
+	// Set to true to enable the auto-healing process, which automatically rebuilds instances when a node fails or is in maintenance mode.
+	EnableAutoHealing bool `json:"enableAutoHealing"`
 	// The number of seconds to wait before starting a rebuild when a node fails.
-	RebuildStartDelaySeconds int32 `json:"rebuildStartDelaySeconds"`
+	RebuildStartDelaySecondsWhenNodeFail int32 `json:"rebuildStartDelaySecondsWhenNodeFail"`
+	// The number of seconds to wait before starting a rebuild when a node is in maintenance mode.
+	RebuildStartDelaySecondsWhenInMaintenance int32 `json:"rebuildStartDelaySecondsWhenInMaintenance"`
 	// The minimum time in seconds between consecutive rebuild jobs for one cluster.
 	MinClusterRebuildIntervalSeconds int32 `json:"minClusterRebuildIntervalSeconds"`
 	// The maximum number of rebuild jobs that can run simultaneously for a single node.
 	RebuildConcurrencyPerNode int32 `json:"rebuildConcurrencyPerNode"`
+	// The maximum number of rebuild jobs that can run simultaneously for the whole k8s cluster.
+	RebuildConcurrencyGlobally int32 `json:"rebuildConcurrencyGlobally"`
 	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
 	UnparsedObject       map[string]interface{} `json:"-"`
 	AdditionalProperties map[string]interface{} `json:"-"`
@@ -27,12 +31,14 @@ type AutohealingConfig struct {
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed.
-func NewAutohealingConfig(pauseAutoHealingOnNodeUnavailable bool, rebuildStartDelaySeconds int32, minClusterRebuildIntervalSeconds int32, rebuildConcurrencyPerNode int32) *AutohealingConfig {
+func NewAutohealingConfig(enableAutoHealing bool, rebuildStartDelaySecondsWhenNodeFail int32, rebuildStartDelaySecondsWhenInMaintenance int32, minClusterRebuildIntervalSeconds int32, rebuildConcurrencyPerNode int32, rebuildConcurrencyGlobally int32) *AutohealingConfig {
 	this := AutohealingConfig{}
-	this.PauseAutoHealingOnNodeUnavailable = pauseAutoHealingOnNodeUnavailable
-	this.RebuildStartDelaySeconds = rebuildStartDelaySeconds
+	this.EnableAutoHealing = enableAutoHealing
+	this.RebuildStartDelaySecondsWhenNodeFail = rebuildStartDelaySecondsWhenNodeFail
+	this.RebuildStartDelaySecondsWhenInMaintenance = rebuildStartDelaySecondsWhenInMaintenance
 	this.MinClusterRebuildIntervalSeconds = minClusterRebuildIntervalSeconds
 	this.RebuildConcurrencyPerNode = rebuildConcurrencyPerNode
+	this.RebuildConcurrencyGlobally = rebuildConcurrencyGlobally
 	return &this
 }
 
@@ -44,50 +50,73 @@ func NewAutohealingConfigWithDefaults() *AutohealingConfig {
 	return &this
 }
 
-// GetPauseAutoHealingOnNodeUnavailable returns the PauseAutoHealingOnNodeUnavailable field value.
-func (o *AutohealingConfig) GetPauseAutoHealingOnNodeUnavailable() bool {
+// GetEnableAutoHealing returns the EnableAutoHealing field value.
+func (o *AutohealingConfig) GetEnableAutoHealing() bool {
 	if o == nil {
 		var ret bool
 		return ret
 	}
-	return o.PauseAutoHealingOnNodeUnavailable
+	return o.EnableAutoHealing
 }
 
-// GetPauseAutoHealingOnNodeUnavailableOk returns a tuple with the PauseAutoHealingOnNodeUnavailable field value
+// GetEnableAutoHealingOk returns a tuple with the EnableAutoHealing field value
 // and a boolean to check if the value has been set.
-func (o *AutohealingConfig) GetPauseAutoHealingOnNodeUnavailableOk() (*bool, bool) {
+func (o *AutohealingConfig) GetEnableAutoHealingOk() (*bool, bool) {
 	if o == nil {
 		return nil, false
 	}
-	return &o.PauseAutoHealingOnNodeUnavailable, true
+	return &o.EnableAutoHealing, true
 }
 
-// SetPauseAutoHealingOnNodeUnavailable sets field value.
-func (o *AutohealingConfig) SetPauseAutoHealingOnNodeUnavailable(v bool) {
-	o.PauseAutoHealingOnNodeUnavailable = v
+// SetEnableAutoHealing sets field value.
+func (o *AutohealingConfig) SetEnableAutoHealing(v bool) {
+	o.EnableAutoHealing = v
 }
 
-// GetRebuildStartDelaySeconds returns the RebuildStartDelaySeconds field value.
-func (o *AutohealingConfig) GetRebuildStartDelaySeconds() int32 {
+// GetRebuildStartDelaySecondsWhenNodeFail returns the RebuildStartDelaySecondsWhenNodeFail field value.
+func (o *AutohealingConfig) GetRebuildStartDelaySecondsWhenNodeFail() int32 {
 	if o == nil {
 		var ret int32
 		return ret
 	}
-	return o.RebuildStartDelaySeconds
+	return o.RebuildStartDelaySecondsWhenNodeFail
 }
 
-// GetRebuildStartDelaySecondsOk returns a tuple with the RebuildStartDelaySeconds field value
+// GetRebuildStartDelaySecondsWhenNodeFailOk returns a tuple with the RebuildStartDelaySecondsWhenNodeFail field value
 // and a boolean to check if the value has been set.
-func (o *AutohealingConfig) GetRebuildStartDelaySecondsOk() (*int32, bool) {
+func (o *AutohealingConfig) GetRebuildStartDelaySecondsWhenNodeFailOk() (*int32, bool) {
 	if o == nil {
 		return nil, false
 	}
-	return &o.RebuildStartDelaySeconds, true
+	return &o.RebuildStartDelaySecondsWhenNodeFail, true
 }
 
-// SetRebuildStartDelaySeconds sets field value.
-func (o *AutohealingConfig) SetRebuildStartDelaySeconds(v int32) {
-	o.RebuildStartDelaySeconds = v
+// SetRebuildStartDelaySecondsWhenNodeFail sets field value.
+func (o *AutohealingConfig) SetRebuildStartDelaySecondsWhenNodeFail(v int32) {
+	o.RebuildStartDelaySecondsWhenNodeFail = v
+}
+
+// GetRebuildStartDelaySecondsWhenInMaintenance returns the RebuildStartDelaySecondsWhenInMaintenance field value.
+func (o *AutohealingConfig) GetRebuildStartDelaySecondsWhenInMaintenance() int32 {
+	if o == nil {
+		var ret int32
+		return ret
+	}
+	return o.RebuildStartDelaySecondsWhenInMaintenance
+}
+
+// GetRebuildStartDelaySecondsWhenInMaintenanceOk returns a tuple with the RebuildStartDelaySecondsWhenInMaintenance field value
+// and a boolean to check if the value has been set.
+func (o *AutohealingConfig) GetRebuildStartDelaySecondsWhenInMaintenanceOk() (*int32, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.RebuildStartDelaySecondsWhenInMaintenance, true
+}
+
+// SetRebuildStartDelaySecondsWhenInMaintenance sets field value.
+func (o *AutohealingConfig) SetRebuildStartDelaySecondsWhenInMaintenance(v int32) {
+	o.RebuildStartDelaySecondsWhenInMaintenance = v
 }
 
 // GetMinClusterRebuildIntervalSeconds returns the MinClusterRebuildIntervalSeconds field value.
@@ -136,16 +165,41 @@ func (o *AutohealingConfig) SetRebuildConcurrencyPerNode(v int32) {
 	o.RebuildConcurrencyPerNode = v
 }
 
+// GetRebuildConcurrencyGlobally returns the RebuildConcurrencyGlobally field value.
+func (o *AutohealingConfig) GetRebuildConcurrencyGlobally() int32 {
+	if o == nil {
+		var ret int32
+		return ret
+	}
+	return o.RebuildConcurrencyGlobally
+}
+
+// GetRebuildConcurrencyGloballyOk returns a tuple with the RebuildConcurrencyGlobally field value
+// and a boolean to check if the value has been set.
+func (o *AutohealingConfig) GetRebuildConcurrencyGloballyOk() (*int32, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.RebuildConcurrencyGlobally, true
+}
+
+// SetRebuildConcurrencyGlobally sets field value.
+func (o *AutohealingConfig) SetRebuildConcurrencyGlobally(v int32) {
+	o.RebuildConcurrencyGlobally = v
+}
+
 // MarshalJSON serializes the struct using spec logic.
 func (o AutohealingConfig) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
 	if o.UnparsedObject != nil {
 		return common.Marshal(o.UnparsedObject)
 	}
-	toSerialize["pauseAutoHealingOnNodeUnavailable"] = o.PauseAutoHealingOnNodeUnavailable
-	toSerialize["rebuildStartDelaySeconds"] = o.RebuildStartDelaySeconds
+	toSerialize["enableAutoHealing"] = o.EnableAutoHealing
+	toSerialize["rebuildStartDelaySecondsWhenNodeFail"] = o.RebuildStartDelaySecondsWhenNodeFail
+	toSerialize["rebuildStartDelaySecondsWhenInMaintenance"] = o.RebuildStartDelaySecondsWhenInMaintenance
 	toSerialize["minClusterRebuildIntervalSeconds"] = o.MinClusterRebuildIntervalSeconds
 	toSerialize["rebuildConcurrencyPerNode"] = o.RebuildConcurrencyPerNode
+	toSerialize["rebuildConcurrencyGlobally"] = o.RebuildConcurrencyGlobally
 
 	for key, value := range o.AdditionalProperties {
 		toSerialize[key] = value
@@ -156,19 +210,24 @@ func (o AutohealingConfig) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON deserializes the given payload.
 func (o *AutohealingConfig) UnmarshalJSON(bytes []byte) (err error) {
 	all := struct {
-		PauseAutoHealingOnNodeUnavailable *bool  `json:"pauseAutoHealingOnNodeUnavailable"`
-		RebuildStartDelaySeconds          *int32 `json:"rebuildStartDelaySeconds"`
-		MinClusterRebuildIntervalSeconds  *int32 `json:"minClusterRebuildIntervalSeconds"`
-		RebuildConcurrencyPerNode         *int32 `json:"rebuildConcurrencyPerNode"`
+		EnableAutoHealing                         *bool  `json:"enableAutoHealing"`
+		RebuildStartDelaySecondsWhenNodeFail      *int32 `json:"rebuildStartDelaySecondsWhenNodeFail"`
+		RebuildStartDelaySecondsWhenInMaintenance *int32 `json:"rebuildStartDelaySecondsWhenInMaintenance"`
+		MinClusterRebuildIntervalSeconds          *int32 `json:"minClusterRebuildIntervalSeconds"`
+		RebuildConcurrencyPerNode                 *int32 `json:"rebuildConcurrencyPerNode"`
+		RebuildConcurrencyGlobally                *int32 `json:"rebuildConcurrencyGlobally"`
 	}{}
 	if err = common.Unmarshal(bytes, &all); err != nil {
 		return common.Unmarshal(bytes, &o.UnparsedObject)
 	}
-	if all.PauseAutoHealingOnNodeUnavailable == nil {
-		return fmt.Errorf("required field pauseAutoHealingOnNodeUnavailable missing")
+	if all.EnableAutoHealing == nil {
+		return fmt.Errorf("required field enableAutoHealing missing")
 	}
-	if all.RebuildStartDelaySeconds == nil {
-		return fmt.Errorf("required field rebuildStartDelaySeconds missing")
+	if all.RebuildStartDelaySecondsWhenNodeFail == nil {
+		return fmt.Errorf("required field rebuildStartDelaySecondsWhenNodeFail missing")
+	}
+	if all.RebuildStartDelaySecondsWhenInMaintenance == nil {
+		return fmt.Errorf("required field rebuildStartDelaySecondsWhenInMaintenance missing")
 	}
 	if all.MinClusterRebuildIntervalSeconds == nil {
 		return fmt.Errorf("required field minClusterRebuildIntervalSeconds missing")
@@ -176,16 +235,21 @@ func (o *AutohealingConfig) UnmarshalJSON(bytes []byte) (err error) {
 	if all.RebuildConcurrencyPerNode == nil {
 		return fmt.Errorf("required field rebuildConcurrencyPerNode missing")
 	}
+	if all.RebuildConcurrencyGlobally == nil {
+		return fmt.Errorf("required field rebuildConcurrencyGlobally missing")
+	}
 	additionalProperties := make(map[string]interface{})
 	if err = common.Unmarshal(bytes, &additionalProperties); err == nil {
-		common.DeleteKeys(additionalProperties, &[]string{"pauseAutoHealingOnNodeUnavailable", "rebuildStartDelaySeconds", "minClusterRebuildIntervalSeconds", "rebuildConcurrencyPerNode"})
+		common.DeleteKeys(additionalProperties, &[]string{"enableAutoHealing", "rebuildStartDelaySecondsWhenNodeFail", "rebuildStartDelaySecondsWhenInMaintenance", "minClusterRebuildIntervalSeconds", "rebuildConcurrencyPerNode", "rebuildConcurrencyGlobally"})
 	} else {
 		return err
 	}
-	o.PauseAutoHealingOnNodeUnavailable = *all.PauseAutoHealingOnNodeUnavailable
-	o.RebuildStartDelaySeconds = *all.RebuildStartDelaySeconds
+	o.EnableAutoHealing = *all.EnableAutoHealing
+	o.RebuildStartDelaySecondsWhenNodeFail = *all.RebuildStartDelaySecondsWhenNodeFail
+	o.RebuildStartDelaySecondsWhenInMaintenance = *all.RebuildStartDelaySecondsWhenInMaintenance
 	o.MinClusterRebuildIntervalSeconds = *all.MinClusterRebuildIntervalSeconds
 	o.RebuildConcurrencyPerNode = *all.RebuildConcurrencyPerNode
+	o.RebuildConcurrencyGlobally = *all.RebuildConcurrencyGlobally
 
 	if len(additionalProperties) > 0 {
 		o.AdditionalProperties = additionalProperties
