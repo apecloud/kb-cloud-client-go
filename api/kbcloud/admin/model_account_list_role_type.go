c6220199d0b13ab0ba0088c57a65b4a6c68d052c
aure-lee <aure-lee@users.noreply.github.com>
2024-11-25 09:43:28 +0000

    chore: auto update clinent api https://github.com/apecloud/apecloud/pull/7925

--- api/kbcloud/admin/model_account_list_role_type.go
+++ api/kbcloud/admin/model_account_list_role_type.go
@@ -6,8 +6,6 @@ package admin
 
 import (
 	"fmt"
-
-	"github.com/apecloud/kb-cloud-client-go/api/common"
 )
 
 // AccountListRoleType The user role name, should be one of [ROOT, SUPERUSER, BASICUSER].
--- api/kbcloud/model_dms_pagination.go
+++ api/kbcloud/model_dms_pagination.go
@@ -4,8 +4,6 @@
 
 package kbcloud
 
-import "github.com/apecloud/kb-cloud-client-go/api/common"
-
 type DmsPagination struct {
 	RowsCount  *int32 `json:"rows_count,omitempty"`
 	Page       *int32 `json:"page,omitempty"`
