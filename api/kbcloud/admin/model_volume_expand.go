24b4246ba27235a56c34c5dff207af6d08dc8b21
ldming <ldming@users.noreply.github.com>
2024-11-27 09:42:13 +0000

    chore: auto update clinent api https://github.com/apecloud/apecloud/pull/7990

--- api/kbcloud/admin/model_volume_expand.go
+++ api/kbcloud/admin/model_volume_expand.go
@@ -4,8 +4,6 @@
 
 package admin
 
-import "github.com/apecloud/kb-cloud-client-go/api/common"
-
 type VolumeExpand struct {
 	// the new volume size of cluster
 	Size *string `json:"size,omitempty"`
--- api/kbcloud/model_backup_create.go
+++ api/kbcloud/model_backup_create.go
@@ -6,8 +6,6 @@ package kbcloud
 
 import (
 	"fmt"
-
-	"github.com/apecloud/kb-cloud-client-go/api/common"
 )
 
 // BackupCreate BackupCreate is the payload to create a KubeBlocks cluster backup
