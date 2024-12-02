50868f59162d98c25f24b8da40ab068d6f255cc4
fengluodb <fengluodb@users.noreply.github.com>
2024-12-02 09:52:02 +0000

    chore: auto update clinent api https://github.com/apecloud/apecloud/pull/8085

--- api/kbcloud/admin/model_storage_class_reclaim_policy.go
+++ api/kbcloud/admin/model_storage_class_reclaim_policy.go
@@ -6,8 +6,6 @@ package admin
 
 import (
 	"fmt"
-
-	"github.com/apecloud/kb-cloud-client-go/api/common"
 )
 
 // StorageClassReclaimPolicy reclaimPolicy controls the reclaimPolicy for dynamically provisioned PersistentVolumes of this storage class. Defaults to Delete.
