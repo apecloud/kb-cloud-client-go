db326b7c5b916436e58fecb5366feb2bb9d73982
aure-lee <aure-lee@users.noreply.github.com>
2024-12-06 02:21:25 +0000

    chore: auto update clinent api https://github.com/apecloud/apecloud/pull/8185

--- api/kbcloud/admin/model_instance_resource_stats.go
+++ api/kbcloud/admin/model_instance_resource_stats.go
@@ -6,8 +6,6 @@ package admin
 
 import (
 	"fmt"
-
-	"github.com/apecloud/kb-cloud-client-go/api/common"
 )
 
 // InstanceResourceStats InstanceResourceStats holds the requests, limits, and available stats for an instance.
