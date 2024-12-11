3ed44e496f475af8c397fa3566f93488418664c7
Chen-speculation <Chen-speculation@users.noreply.github.com>
2024-12-11 08:31:25 +0000

    chore: auto update clinent api https://github.com/apecloud/apecloud/pull/8284

--- api/kbcloud/admin/model_environment_backup_repo.go
+++ api/kbcloud/admin/model_environment_backup_repo.go
@@ -4,8 +4,6 @@
 
 package admin
 
-import "github.com/apecloud/kb-cloud-client-go/api/common"
-
 type EnvironmentBackupRepo struct {
 	// backup repo list
 	Items []RawBackupRepo `json:"items,omitempty"`
--- api/kbcloud/admin/model_static_cluster.go
+++ api/kbcloud/admin/model_static_cluster.go
@@ -4,8 +4,6 @@
 
 package admin
 
-import "github.com/apecloud/kb-cloud-client-go/api/common"
-
 type StaticCluster struct {
 	// the number of replicas
 	Replicas *int32 `json:"replicas,omitempty"`
