8b12273ad8cd0414d212b10cc175eebf93f5ea63
fantasy-yzj <fantasy-yzj@users.noreply.github.com>
2024-12-23 08:16:53 +0000

    chore: auto update clinent api https://github.com/apecloud/apecloud/pull/8568

--- api/kbcloud/admin/model_ops_expose_ports_mapping_item.go
+++ api/kbcloud/admin/model_ops_expose_ports_mapping_item.go
@@ -4,8 +4,6 @@
 
 package admin
 
-import "github.com/apecloud/kb-cloud-client-go/api/common"
-
 type OpsExposePortsMappingItem struct {
 	Old *int32 `json:"old,omitempty"`
 	New *int32 `json:"new,omitempty"`
