317a8d99838cec3e6ff0b951a7e424fc50960213
Chen-speculation <Chen-speculation@users.noreply.github.com>
2025-01-15 08:42:34 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/9161

--- api/kbcloud/admin/model_environment_module.go
+++ api/kbcloud/admin/model_environment_module.go
@@ -16,7 +16,7 @@ type EnvironmentModule struct {
 	Name string `json:"name"`
 	// Environment module version
 	Version *string `json:"version,omitempty"`
-	// Environment module status (running,updating, error, not_installed and status for cluster)
+	// Environment module status (running,updating, error, disabled and status for cluster)
 	Status string `json:"status"`
 	// Hosting status (Hostable, Non-hostable, Hosted). When hosting_status is Hosted, cluster_info will be returned
 	HostingStatus *HostingStatus `json:"hostingStatus,omitempty"`
