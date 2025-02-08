70d54e605e1f1735eb701c6b3c9dc5b0fcc59510
kizuna-lek <kizuna-lek@users.noreply.github.com>
2025-02-08 04:39:31 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/9301

--- api/kbcloud/admin/api_param_tpl.go
+++ api/kbcloud/admin/api_param_tpl.go
@@ -239,6 +239,75 @@ func (a *ParamTplApi) DeleteParamTpl(ctx _context.Context, orgName string, param
 	return localVarHTTPResponse, nil
 }
 
+// DeleteParamTplV2 Delete configuration template.
+func (a *ParamTplApi) DeleteParamTplV2(ctx _context.Context, orgName string, paramTplName string) (*_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod = _nethttp.MethodDelete
+		localVarPostBody   interface{}
+	)
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "paramTpl",
+		OperationID: "deleteParamTplV2",
+		Path:        "/admin/v1/paramTplsV2/{paramTplName}",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".ParamTplApi.DeleteParamTplV2")
+	if err != nil {
+		return nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/paramTplsV2/{paramTplName}"
+	localVarPath = strings.Replace(localVarPath, "{"+"paramTplName"+"}", _neturl.PathEscape(common.ParameterToString(paramTplName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarQueryParams.Add("orgName", common.ParameterToString(orgName, ""))
+	localVarHeaderParams["Accept"] = "application/json"
+
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"BearerToken", "authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarHTTPResponse, newErr
+	}
+
+	return localVarHTTPResponse, nil
+}
+
 // GetClusterParamTplsOptionalParameters holds optional parameters for GetClusterParamTpls.
 type GetClusterParamTplsOptionalParameters struct {
 	Component  *string
@@ -584,6 +653,89 @@ func (a *ParamTplApi) PatchParamTpl(ctx _context.Context, orgName string, paramT
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+// PatchParamTplV2 Update configuration template.
+// partially update the parameter template
+func (a *ParamTplApi) PatchParamTplV2(ctx _context.Context, orgName string, paramTplName string, body ParamTplUpdate) (ParamTplListItem, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodPatch
+		localVarPostBody    interface{}
+		localVarReturnValue ParamTplListItem
+	)
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "paramTpl",
+		OperationID: "patchParamTplV2",
+		Path:        "/admin/v1/paramTplsV2/{paramTplName}",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".ParamTplApi.PatchParamTplV2")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/paramTplsV2/{paramTplName}"
+	localVarPath = strings.Replace(localVarPath, "{"+"paramTplName"+"}", _neturl.PathEscape(common.ParameterToString(paramTplName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarQueryParams.Add("orgName", common.ParameterToString(orgName, ""))
+	localVarHeaderParams["Content-Type"] = "application/json"
+	localVarHeaderParams["Accept"] = "application/json"
+
+	// body params
+	localVarPostBody = &body
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"BearerToken", "authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 // ReadParamTplOptionalParameters holds optional parameters for ReadParamTpl.
 type ReadParamTplOptionalParameters struct {
 	Partition *ParamTplPartition
@@ -632,6 +784,113 @@ func (a *ParamTplApi) ReadParamTpl(ctx _context.Context, orgName string, paramTp
 	}
 
 	localVarPath := localBasePath + "/admin/v1/paramTpls/{paramTplName}"
+	localVarPath = strings.Replace(localVarPath, "{"+"paramTplName"+"}", _neturl.PathEscape(common.ParameterToString(paramTplName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarQueryParams.Add("orgName", common.ParameterToString(orgName, ""))
+	if optionalParams.Partition != nil {
+		localVarQueryParams.Add("partition", common.ParameterToString(*optionalParams.Partition, ""))
+	}
+	localVarHeaderParams["Accept"] = "application/json"
+
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"BearerToken", "authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+// ReadParamTplV2OptionalParameters holds optional parameters for ReadParamTplV2.
+type ReadParamTplV2OptionalParameters struct {
+	Partition *ParamTplPartition
+}
+
+// NewReadParamTplV2OptionalParameters creates an empty struct for parameters.
+func NewReadParamTplV2OptionalParameters() *ReadParamTplV2OptionalParameters {
+	this := ReadParamTplV2OptionalParameters{}
+	return &this
+}
+
+// WithPartition sets the corresponding parameter name and returns the struct.
+func (r *ReadParamTplV2OptionalParameters) WithPartition(partition ParamTplPartition) *ReadParamTplV2OptionalParameters {
+	r.Partition = &partition
+	return r
+}
+
+// ReadParamTplV2 Get parameter template details.
+func (a *ParamTplApi) ReadParamTplV2(ctx _context.Context, orgName string, paramTplName string, o ...ReadParamTplV2OptionalParameters) (ParameterPropList, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodGet
+		localVarPostBody    interface{}
+		localVarReturnValue ParameterPropList
+		optionalParams      ReadParamTplV2OptionalParameters
+	)
+
+	if len(o) > 1 {
+		return localVarReturnValue, nil, common.ReportError("only one argument of type ReadParamTplV2OptionalParameters is allowed")
+	}
+	if len(o) == 1 {
+		optionalParams = o[0]
+	}
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "paramTpl",
+		OperationID: "readParamTplV2",
+		Path:        "/admin/v1/paramTplsV2/{paramTplName}",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".ParamTplApi.ReadParamTplV2")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/paramTplsV2/{paramTplName}"
 	localVarPath = strings.Replace(localVarPath, "{"+"paramTplName"+"}", _neturl.PathEscape(common.ParameterToString(paramTplName, "")), -1)
 
 	localVarHeaderParams := make(map[string]string)
