83de6338f89262d01f7750b41ac75774c59e96a9
aure-lee <aure-lee@users.noreply.github.com>
2025-02-18 09:03:48 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/9687

--- api/kbcloud/api_event.go
+++ api/kbcloud/api_event.go
@@ -99,13 +99,17 @@ func (a *EventApi) GetEvent(ctx _context.Context, orgName string, eventId string
 
 // ListEventsOptionalParameters holds optional parameters for ListEvents.
 type ListEventsOptionalParameters struct {
-	ResourceId   *int32
-	ResourceType *string
-	EventName    *string
-	OperatorId   *int32
-	PageNumber   *int32
-	PageSize     *int32
-	OrderBy      *string
+	ResourceId    *int32
+	ResourceType  *string
+	EventName     *string
+	OperatorId    *int32
+	ResourceIds   *[]string
+	ResourceTypes *[]string
+	EventNames    *[]string
+	OperatorIds   *[]int32
+	PageNumber    *int32
+	PageSize      *int32
+	OrderBy       *string
 }
 
 // NewListEventsOptionalParameters creates an empty struct for parameters.
@@ -135,6 +139,30 @@ func (r *ListEventsOptionalParameters) WithEventName(eventName string) *ListEven
 // WithOperatorId sets the corresponding parameter name and returns the struct.
 func (r *ListEventsOptionalParameters) WithOperatorId(operatorId int32) *ListEventsOptionalParameters {
 	r.OperatorId = &operatorId
+	return r
+}
+
+// WithResourceIds sets the corresponding parameter name and returns the struct.
+func (r *ListEventsOptionalParameters) WithResourceIds(resourceIds []string) *ListEventsOptionalParameters {
+	r.ResourceIds = &resourceIds
+	return r
+}
+
+// WithResourceTypes sets the corresponding parameter name and returns the struct.
+func (r *ListEventsOptionalParameters) WithResourceTypes(resourceTypes []string) *ListEventsOptionalParameters {
+	r.ResourceTypes = &resourceTypes
+	return r
+}
+
+// WithEventNames sets the corresponding parameter name and returns the struct.
+func (r *ListEventsOptionalParameters) WithEventNames(eventNames []string) *ListEventsOptionalParameters {
+	r.EventNames = &eventNames
+	return r
+}
+
+// WithOperatorIds sets the corresponding parameter name and returns the struct.
+func (r *ListEventsOptionalParameters) WithOperatorIds(operatorIds []int32) *ListEventsOptionalParameters {
+	r.OperatorIds = &operatorIds
 	return r
 }
 
@@ -206,6 +234,18 @@ func (a *EventApi) ListEvents(ctx _context.Context, orgName string, start int64,
 	}
 	if optionalParams.OperatorId != nil {
 		localVarQueryParams.Add("operatorId", common.ParameterToString(*optionalParams.OperatorId, ""))
+	}
+	if optionalParams.ResourceIds != nil {
+		localVarQueryParams.Add("resourceIds", common.ParameterToString(*optionalParams.ResourceIds, "csv"))
+	}
+	if optionalParams.ResourceTypes != nil {
+		localVarQueryParams.Add("resourceTypes", common.ParameterToString(*optionalParams.ResourceTypes, "csv"))
+	}
+	if optionalParams.EventNames != nil {
+		localVarQueryParams.Add("eventNames", common.ParameterToString(*optionalParams.EventNames, "csv"))
+	}
+	if optionalParams.OperatorIds != nil {
+		localVarQueryParams.Add("operatorIds", common.ParameterToString(*optionalParams.OperatorIds, "csv"))
 	}
 	if optionalParams.PageNumber != nil {
 		localVarQueryParams.Add("pageNumber", common.ParameterToString(*optionalParams.PageNumber, ""))
