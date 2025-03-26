30cce4e63127972bc3eaa67baeca3b93f0358af3
kizuna-lek <kizuna-lek@users.noreply.github.com>
2025-03-26 02:38:54 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/10678

--- api/kbcloud/admin/api_environment.go
+++ api/kbcloud/admin/api_environment.go
@@ -101,6 +101,113 @@ func (a *EnvironmentApi) AddNodes(ctx _context.Context, environmentName string, 
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+// CheckKubeconfigOptionalParameters holds optional parameters for CheckKubeconfig.
+type CheckKubeconfigOptionalParameters struct {
+	Body *string
+}
+
+// NewCheckKubeconfigOptionalParameters creates an empty struct for parameters.
+func NewCheckKubeconfigOptionalParameters() *CheckKubeconfigOptionalParameters {
+	this := CheckKubeconfigOptionalParameters{}
+	return &this
+}
+
+// WithBody sets the corresponding parameter name and returns the struct.
+func (r *CheckKubeconfigOptionalParameters) WithBody(body string) *CheckKubeconfigOptionalParameters {
+	r.Body = &body
+	return r
+}
+
+// CheckKubeconfig check kubeconfig.
+func (a *EnvironmentApi) CheckKubeconfig(ctx _context.Context, o ...CheckKubeconfigOptionalParameters) (HttpBody, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodPost
+		localVarPostBody    interface{}
+		localVarReturnValue HttpBody
+		optionalParams      CheckKubeconfigOptionalParameters
+	)
+
+	if len(o) > 1 {
+		return localVarReturnValue, nil, common.ReportError("only one argument of type CheckKubeconfigOptionalParameters is allowed")
+	}
+	if len(o) == 1 {
+		optionalParams = o[0]
+	}
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "environment",
+		OperationID: "checkKubeconfig",
+		Path:        "/admin/v1/checkKubeconfig",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".EnvironmentApi.CheckKubeconfig")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/checkKubeconfig"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarHeaderParams["Content-Type"] = "application/json"
+	localVarHeaderParams["Accept"] = "application/json"
+
+	// body params
+	if optionalParams.Body != nil {
+		localVarPostBody = &optionalParams.Body
+	}
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"BearerToken", "authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 // CheckNodeSSHConfig Check nodes ssh config.
 // Check ssh config of provided IP addresses
 func (a *EnvironmentApi) CheckNodeSSHConfig(ctx _context.Context, body SshConfigCheckRequest) (SshConfigCheckResponse, *_nethttp.Response, error) {
