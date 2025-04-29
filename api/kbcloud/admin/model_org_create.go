c09eaf691f10e0ca32f0b49e6e17a15e8b201d60
heng4fun <heng4fun@users.noreply.github.com>
2025-04-29 06:51:37 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/11336

---
+++ api/kbcloud/admin/model_org_create.go
@@ -0,0 +1,206 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at ApeCloud (https://www.apecloud.com/).
+// Copyright 2022-Present ApeCloud Co., Ltd
+
+package admin
+
+import (
+	"fmt"
+
+	"github.com/apecloud/kb-cloud-client-go/api/common"
+)
+
+// OrgCreate OrgCreate is the payload for organization creation
+type OrgCreate struct {
+	// The description of the organization
+	Description *string `json:"description,omitempty"`
+	// The display name of the organization
+	DisplayName *string `json:"displayName,omitempty"`
+	// The full, unique name of this Object in the format organizations/{name}, set during creation. name must be a valid RFC 1123 compliant DNS label
+	Name string `json:"name"`
+	// The id of the creator of the organization
+	Creator *int64 `json:"creator,omitempty"`
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject       map[string]interface{} `json:"-"`
+	AdditionalProperties map[string]interface{} `json:"-"`
+}
+
+// NewOrgCreate instantiates a new OrgCreate object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewOrgCreate(name string) *OrgCreate {
+	this := OrgCreate{}
+	this.Name = name
+	return &this
+}
+
+// NewOrgCreateWithDefaults instantiates a new OrgCreate object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewOrgCreateWithDefaults() *OrgCreate {
+	this := OrgCreate{}
+	return &this
+}
+
+// GetDescription returns the Description field value if set, zero value otherwise.
+func (o *OrgCreate) GetDescription() string {
+	if o == nil || o.Description == nil {
+		var ret string
+		return ret
+	}
+	return *o.Description
+}
+
+// GetDescriptionOk returns a tuple with the Description field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *OrgCreate) GetDescriptionOk() (*string, bool) {
+	if o == nil || o.Description == nil {
+		return nil, false
+	}
+	return o.Description, true
+}
+
+// HasDescription returns a boolean if a field has been set.
+func (o *OrgCreate) HasDescription() bool {
+	return o != nil && o.Description != nil
+}
+
+// SetDescription gets a reference to the given string and assigns it to the Description field.
+func (o *OrgCreate) SetDescription(v string) {
+	o.Description = &v
+}
+
+// GetDisplayName returns the DisplayName field value if set, zero value otherwise.
+func (o *OrgCreate) GetDisplayName() string {
+	if o == nil || o.DisplayName == nil {
+		var ret string
+		return ret
+	}
+	return *o.DisplayName
+}
+
+// GetDisplayNameOk returns a tuple with the DisplayName field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *OrgCreate) GetDisplayNameOk() (*string, bool) {
+	if o == nil || o.DisplayName == nil {
+		return nil, false
+	}
+	return o.DisplayName, true
+}
+
+// HasDisplayName returns a boolean if a field has been set.
+func (o *OrgCreate) HasDisplayName() bool {
+	return o != nil && o.DisplayName != nil
+}
+
+// SetDisplayName gets a reference to the given string and assigns it to the DisplayName field.
+func (o *OrgCreate) SetDisplayName(v string) {
+	o.DisplayName = &v
+}
+
+// GetName returns the Name field value.
+func (o *OrgCreate) GetName() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+	return o.Name
+}
+
+// GetNameOk returns a tuple with the Name field value
+// and a boolean to check if the value has been set.
+func (o *OrgCreate) GetNameOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Name, true
+}
+
+// SetName sets field value.
+func (o *OrgCreate) SetName(v string) {
+	o.Name = v
+}
+
+// GetCreator returns the Creator field value if set, zero value otherwise.
+func (o *OrgCreate) GetCreator() int64 {
+	if o == nil || o.Creator == nil {
+		var ret int64
+		return ret
+	}
+	return *o.Creator
+}
+
+// GetCreatorOk returns a tuple with the Creator field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *OrgCreate) GetCreatorOk() (*int64, bool) {
+	if o == nil || o.Creator == nil {
+		return nil, false
+	}
+	return o.Creator, true
+}
+
+// HasCreator returns a boolean if a field has been set.
+func (o *OrgCreate) HasCreator() bool {
+	return o != nil && o.Creator != nil
+}
+
+// SetCreator gets a reference to the given int64 and assigns it to the Creator field.
+func (o *OrgCreate) SetCreator(v int64) {
+	o.Creator = &v
+}
+
+// MarshalJSON serializes the struct using spec logic.
+func (o OrgCreate) MarshalJSON() ([]byte, error) {
+	toSerialize := map[string]interface{}{}
+	if o.UnparsedObject != nil {
+		return common.Marshal(o.UnparsedObject)
+	}
+	if o.Description != nil {
+		toSerialize["description"] = o.Description
+	}
+	if o.DisplayName != nil {
+		toSerialize["displayName"] = o.DisplayName
+	}
+	toSerialize["name"] = o.Name
+	if o.Creator != nil {
+		toSerialize["creator"] = o.Creator
+	}
+
+	for key, value := range o.AdditionalProperties {
+		toSerialize[key] = value
+	}
+	return common.Marshal(toSerialize)
+}
+
+// UnmarshalJSON deserializes the given payload.
+func (o *OrgCreate) UnmarshalJSON(bytes []byte) (err error) {
+	all := struct {
+		Description *string `json:"description,omitempty"`
+		DisplayName *string `json:"displayName,omitempty"`
+		Name        *string `json:"name"`
+		Creator     *int64  `json:"creator,omitempty"`
+	}{}
+	if err = common.Unmarshal(bytes, &all); err != nil {
+		return err
+	}
+	if all.Name == nil {
+		return fmt.Errorf("required field name missing")
+	}
+	additionalProperties := make(map[string]interface{})
+	if err = common.Unmarshal(bytes, &additionalProperties); err == nil {
+		common.DeleteKeys(additionalProperties, &[]string{"description", "displayName", "name", "creator"})
+	} else {
+		return err
+	}
+	o.Description = all.Description
+	o.DisplayName = all.DisplayName
+	o.Name = *all.Name
+	o.Creator = all.Creator
+
+	if len(additionalProperties) > 0 {
+		o.AdditionalProperties = additionalProperties
+	}
+
+	return nil
+}
