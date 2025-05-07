5f3655c5dbee4968c330c51953003cf0982d1e13
1aal <1aal@users.noreply.github.com>
2025-05-07 01:57:23 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/11262

--- api/kbcloud/api_database.go
+++ api/kbcloud/api_database.go
@@ -309,6 +309,87 @@ func (a *DatabaseApi) DeleteDatabaseOld(ctx _context.Context, orgName string, cl
 	return localVarHTTPResponse, nil
 }
 
+// DeleteMssqlDatabase Delete mssql database.
+// delete mssql database
+func (a *DatabaseApi) DeleteMssqlDatabase(ctx _context.Context, orgName string, clusterName string, databaseName string) (ClusterTask, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodDelete
+		localVarPostBody    interface{}
+		localVarReturnValue ClusterTask
+	)
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "database",
+		OperationID: "deleteMssqlDatabase",
+		Path:        "/api/v1/data/mssql/organizations/{orgName}/clusters/{clusterName}/databases/{databaseName}",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".DatabaseApi.DeleteMssqlDatabase")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/api/v1/data/mssql/organizations/{orgName}/clusters/{clusterName}/databases/{databaseName}"
+	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"clusterName"+"}", _neturl.PathEscape(common.ParameterToString(clusterName, "")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"databaseName"+"}", _neturl.PathEscape(common.ParameterToString(databaseName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarHeaderParams["Accept"] = "application/json"
+
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"BearerToken", "authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 // ListDatabases List cluster databases.
 // list databases for rdbms engine cluster
 func (a *DatabaseApi) ListDatabases(ctx _context.Context, engineName string, orgName string, clusterName string) (DatabaseList, *_nethttp.Response, error) {
