cf78a7f6f84edfa011829326503d39710dca9365
heng4fun <heng4fun@users.noreply.github.com>
2025-08-11 10:13:15 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/12919

---
+++ api/kbcloud/admin/model_mode_option_values_mappings.go
@@ -0,0 +1,132 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at ApeCloud (https://www.apecloud.com/).
+// Copyright 2022-Present ApeCloud Co., Ltd
+
+package admin
+
+import "github.com/apecloud/kb-cloud-client-go/api/common"
+
+type ModeOptionValuesMappings struct {
+	CompatibleKbVersions []ModeCompatibleKubeblocksVersion      `json:"compatibleKBVersions,omitempty"`
+	Mappings             []ModeOptionValuesMappingsMappingsItem `json:"mappings,omitempty"`
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject       map[string]interface{} `json:"-"`
+	AdditionalProperties map[string]interface{} `json:"-"`
+}
+
+// NewModeOptionValuesMappings instantiates a new ModeOptionValuesMappings object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewModeOptionValuesMappings() *ModeOptionValuesMappings {
+	this := ModeOptionValuesMappings{}
+	return &this
+}
+
+// NewModeOptionValuesMappingsWithDefaults instantiates a new ModeOptionValuesMappings object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewModeOptionValuesMappingsWithDefaults() *ModeOptionValuesMappings {
+	this := ModeOptionValuesMappings{}
+	return &this
+}
+
+// GetCompatibleKbVersions returns the CompatibleKbVersions field value if set, zero value otherwise.
+func (o *ModeOptionValuesMappings) GetCompatibleKbVersions() []ModeCompatibleKubeblocksVersion {
+	if o == nil || o.CompatibleKbVersions == nil {
+		var ret []ModeCompatibleKubeblocksVersion
+		return ret
+	}
+	return o.CompatibleKbVersions
+}
+
+// GetCompatibleKbVersionsOk returns a tuple with the CompatibleKbVersions field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ModeOptionValuesMappings) GetCompatibleKbVersionsOk() (*[]ModeCompatibleKubeblocksVersion, bool) {
+	if o == nil || o.CompatibleKbVersions == nil {
+		return nil, false
+	}
+	return &o.CompatibleKbVersions, true
+}
+
+// HasCompatibleKbVersions returns a boolean if a field has been set.
+func (o *ModeOptionValuesMappings) HasCompatibleKbVersions() bool {
+	return o != nil && o.CompatibleKbVersions != nil
+}
+
+// SetCompatibleKbVersions gets a reference to the given []ModeCompatibleKubeblocksVersion and assigns it to the CompatibleKbVersions field.
+func (o *ModeOptionValuesMappings) SetCompatibleKbVersions(v []ModeCompatibleKubeblocksVersion) {
+	o.CompatibleKbVersions = v
+}
+
+// GetMappings returns the Mappings field value if set, zero value otherwise.
+func (o *ModeOptionValuesMappings) GetMappings() []ModeOptionValuesMappingsMappingsItem {
+	if o == nil || o.Mappings == nil {
+		var ret []ModeOptionValuesMappingsMappingsItem
+		return ret
+	}
+	return o.Mappings
+}
+
+// GetMappingsOk returns a tuple with the Mappings field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ModeOptionValuesMappings) GetMappingsOk() (*[]ModeOptionValuesMappingsMappingsItem, bool) {
+	if o == nil || o.Mappings == nil {
+		return nil, false
+	}
+	return &o.Mappings, true
+}
+
+// HasMappings returns a boolean if a field has been set.
+func (o *ModeOptionValuesMappings) HasMappings() bool {
+	return o != nil && o.Mappings != nil
+}
+
+// SetMappings gets a reference to the given []ModeOptionValuesMappingsMappingsItem and assigns it to the Mappings field.
+func (o *ModeOptionValuesMappings) SetMappings(v []ModeOptionValuesMappingsMappingsItem) {
+	o.Mappings = v
+}
+
+// MarshalJSON serializes the struct using spec logic.
+func (o ModeOptionValuesMappings) MarshalJSON() ([]byte, error) {
+	toSerialize := map[string]interface{}{}
+	if o.UnparsedObject != nil {
+		return common.Marshal(o.UnparsedObject)
+	}
+	if o.CompatibleKbVersions != nil {
+		toSerialize["compatibleKBVersions"] = o.CompatibleKbVersions
+	}
+	if o.Mappings != nil {
+		toSerialize["mappings"] = o.Mappings
+	}
+
+	for key, value := range o.AdditionalProperties {
+		toSerialize[key] = value
+	}
+	return common.Marshal(toSerialize)
+}
+
+// UnmarshalJSON deserializes the given payload.
+func (o *ModeOptionValuesMappings) UnmarshalJSON(bytes []byte) (err error) {
+	all := struct {
+		CompatibleKbVersions []ModeCompatibleKubeblocksVersion      `json:"compatibleKBVersions,omitempty"`
+		Mappings             []ModeOptionValuesMappingsMappingsItem `json:"mappings,omitempty"`
+	}{}
+	if err = common.Unmarshal(bytes, &all); err != nil {
+		return err
+	}
+	additionalProperties := make(map[string]interface{})
+	if err = common.Unmarshal(bytes, &additionalProperties); err == nil {
+		common.DeleteKeys(additionalProperties, &[]string{"compatibleKBVersions", "mappings"})
+	} else {
+		return err
+	}
+	o.CompatibleKbVersions = all.CompatibleKbVersions
+	o.Mappings = all.Mappings
+
+	if len(additionalProperties) > 0 {
+		o.AdditionalProperties = additionalProperties
+	}
+
+	return nil
+}
---
+++ api/kbcloud/model_mode_option_values_mappings_mappings_item.go
@@ -0,0 +1,229 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at ApeCloud (https://www.apecloud.com/).
+// Copyright 2022-Present ApeCloud Co., Ltd
+
+package kbcloud
+
+import (
+	"fmt"
+
+	"github.com/apecloud/kb-cloud-client-go/api/common"
+)
+
+type ModeOptionValuesMappingsMappingsItem struct {
+	Component   string                 `json:"component"`
+	SourceKey   string                 `json:"sourceKey"`
+	SourceValue map[string]interface{} `json:"sourceValue,omitempty"`
+	TargetKey   string                 `json:"targetKey"`
+	TargetValue map[string]interface{} `json:"targetValue,omitempty"`
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject       map[string]interface{} `json:"-"`
+	AdditionalProperties map[string]interface{} `json:"-"`
+}
+
+// NewModeOptionValuesMappingsMappingsItem instantiates a new ModeOptionValuesMappingsMappingsItem object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewModeOptionValuesMappingsMappingsItem(component string, sourceKey string, targetKey string) *ModeOptionValuesMappingsMappingsItem {
+	this := ModeOptionValuesMappingsMappingsItem{}
+	this.Component = component
+	this.SourceKey = sourceKey
+	this.TargetKey = targetKey
+	return &this
+}
+
+// NewModeOptionValuesMappingsMappingsItemWithDefaults instantiates a new ModeOptionValuesMappingsMappingsItem object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewModeOptionValuesMappingsMappingsItemWithDefaults() *ModeOptionValuesMappingsMappingsItem {
+	this := ModeOptionValuesMappingsMappingsItem{}
+	return &this
+}
+
+// GetComponent returns the Component field value.
+func (o *ModeOptionValuesMappingsMappingsItem) GetComponent() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+	return o.Component
+}
+
+// GetComponentOk returns a tuple with the Component field value
+// and a boolean to check if the value has been set.
+func (o *ModeOptionValuesMappingsMappingsItem) GetComponentOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Component, true
+}
+
+// SetComponent sets field value.
+func (o *ModeOptionValuesMappingsMappingsItem) SetComponent(v string) {
+	o.Component = v
+}
+
+// GetSourceKey returns the SourceKey field value.
+func (o *ModeOptionValuesMappingsMappingsItem) GetSourceKey() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+	return o.SourceKey
+}
+
+// GetSourceKeyOk returns a tuple with the SourceKey field value
+// and a boolean to check if the value has been set.
+func (o *ModeOptionValuesMappingsMappingsItem) GetSourceKeyOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.SourceKey, true
+}
+
+// SetSourceKey sets field value.
+func (o *ModeOptionValuesMappingsMappingsItem) SetSourceKey(v string) {
+	o.SourceKey = v
+}
+
+// GetSourceValue returns the SourceValue field value if set, zero value otherwise.
+func (o *ModeOptionValuesMappingsMappingsItem) GetSourceValue() map[string]interface{} {
+	if o == nil || o.SourceValue == nil {
+		var ret map[string]interface{}
+		return ret
+	}
+	return o.SourceValue
+}
+
+// GetSourceValueOk returns a tuple with the SourceValue field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ModeOptionValuesMappingsMappingsItem) GetSourceValueOk() (*map[string]interface{}, bool) {
+	if o == nil || o.SourceValue == nil {
+		return nil, false
+	}
+	return &o.SourceValue, true
+}
+
+// HasSourceValue returns a boolean if a field has been set.
+func (o *ModeOptionValuesMappingsMappingsItem) HasSourceValue() bool {
+	return o != nil && o.SourceValue != nil
+}
+
+// SetSourceValue gets a reference to the given map[string]interface{} and assigns it to the SourceValue field.
+func (o *ModeOptionValuesMappingsMappingsItem) SetSourceValue(v map[string]interface{}) {
+	o.SourceValue = v
+}
+
+// GetTargetKey returns the TargetKey field value.
+func (o *ModeOptionValuesMappingsMappingsItem) GetTargetKey() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+	return o.TargetKey
+}
+
+// GetTargetKeyOk returns a tuple with the TargetKey field value
+// and a boolean to check if the value has been set.
+func (o *ModeOptionValuesMappingsMappingsItem) GetTargetKeyOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.TargetKey, true
+}
+
+// SetTargetKey sets field value.
+func (o *ModeOptionValuesMappingsMappingsItem) SetTargetKey(v string) {
+	o.TargetKey = v
+}
+
+// GetTargetValue returns the TargetValue field value if set, zero value otherwise.
+func (o *ModeOptionValuesMappingsMappingsItem) GetTargetValue() map[string]interface{} {
+	if o == nil || o.TargetValue == nil {
+		var ret map[string]interface{}
+		return ret
+	}
+	return o.TargetValue
+}
+
+// GetTargetValueOk returns a tuple with the TargetValue field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ModeOptionValuesMappingsMappingsItem) GetTargetValueOk() (*map[string]interface{}, bool) {
+	if o == nil || o.TargetValue == nil {
+		return nil, false
+	}
+	return &o.TargetValue, true
+}
+
+// HasTargetValue returns a boolean if a field has been set.
+func (o *ModeOptionValuesMappingsMappingsItem) HasTargetValue() bool {
+	return o != nil && o.TargetValue != nil
+}
+
+// SetTargetValue gets a reference to the given map[string]interface{} and assigns it to the TargetValue field.
+func (o *ModeOptionValuesMappingsMappingsItem) SetTargetValue(v map[string]interface{}) {
+	o.TargetValue = v
+}
+
+// MarshalJSON serializes the struct using spec logic.
+func (o ModeOptionValuesMappingsMappingsItem) MarshalJSON() ([]byte, error) {
+	toSerialize := map[string]interface{}{}
+	if o.UnparsedObject != nil {
+		return common.Marshal(o.UnparsedObject)
+	}
+	toSerialize["component"] = o.Component
+	toSerialize["sourceKey"] = o.SourceKey
+	if o.SourceValue != nil {
+		toSerialize["sourceValue"] = o.SourceValue
+	}
+	toSerialize["targetKey"] = o.TargetKey
+	if o.TargetValue != nil {
+		toSerialize["targetValue"] = o.TargetValue
+	}
+
+	for key, value := range o.AdditionalProperties {
+		toSerialize[key] = value
+	}
+	return common.Marshal(toSerialize)
+}
+
+// UnmarshalJSON deserializes the given payload.
+func (o *ModeOptionValuesMappingsMappingsItem) UnmarshalJSON(bytes []byte) (err error) {
+	all := struct {
+		Component   *string                `json:"component"`
+		SourceKey   *string                `json:"sourceKey"`
+		SourceValue map[string]interface{} `json:"sourceValue,omitempty"`
+		TargetKey   *string                `json:"targetKey"`
+		TargetValue map[string]interface{} `json:"targetValue,omitempty"`
+	}{}
+	if err = common.Unmarshal(bytes, &all); err != nil {
+		return err
+	}
+	if all.Component == nil {
+		return fmt.Errorf("required field component missing")
+	}
+	if all.SourceKey == nil {
+		return fmt.Errorf("required field sourceKey missing")
+	}
+	if all.TargetKey == nil {
+		return fmt.Errorf("required field targetKey missing")
+	}
+	additionalProperties := make(map[string]interface{})
+	if err = common.Unmarshal(bytes, &additionalProperties); err == nil {
+		common.DeleteKeys(additionalProperties, &[]string{"component", "sourceKey", "sourceValue", "targetKey", "targetValue"})
+	} else {
+		return err
+	}
+	o.Component = *all.Component
+	o.SourceKey = *all.SourceKey
+	o.SourceValue = all.SourceValue
+	o.TargetKey = *all.TargetKey
+	o.TargetValue = all.TargetValue
+
+	if len(additionalProperties) > 0 {
+		o.AdditionalProperties = additionalProperties
+	}
+
+	return nil
+}
