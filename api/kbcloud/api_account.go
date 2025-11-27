46224bf458735fc47530a88cc5c7390b708032cb
1aal <1aal@users.noreply.github.com>
2025-11-27 09:47:04 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/15034

--- api/kbcloud/api_account.go
+++ api/kbcloud/api_account.go
@@ -101,30 +101,30 @@ func (a *AccountApi) CreateAccount(ctx _context.Context, engineName string, orgN
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
-// CreateAccountOld Create cluster account.
-// create an account in cluster
-// Deprecated: This API is deprecated.
-func (a *AccountApi) CreateAccountOld(ctx _context.Context, orgName string, clusterName string, body Account) (*_nethttp.Response, error) {
+// CreateHiveAccount Create Hive account.
+// create an hive account
+func (a *AccountApi) CreateHiveAccount(ctx _context.Context, orgName string, clusterName string, body Account) (Account, *_nethttp.Response, error) {
 	var (
-		localVarHTTPMethod = _nethttp.MethodPost
-		localVarPostBody   interface{}
+		localVarHTTPMethod  = _nethttp.MethodPost
+		localVarPostBody    interface{}
+		localVarReturnValue Account
 	)
 
 	// Add api info to context
 	apiInfo := common.APIInfo{
 		Tag:         "account",
-		OperationID: "createAccountOld",
-		Path:        "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts",
+		OperationID: "createHiveAccount",
+		Path:        "/api/v1/data/hive/organizations/{orgName}/clusters/{clusterName}/accounts",
 		Version:     "",
 	}
 	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
 
-	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.CreateAccountOld")
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.CreateHiveAccount")
 	if err != nil {
-		return nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
 	}
 
-	localVarPath := localBasePath + "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts"
+	localVarPath := localBasePath + "/api/v1/data/hive/organizations/{orgName}/clusters/{clusterName}/accounts"
 	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"clusterName"+"}", _neturl.PathEscape(common.ParameterToString(clusterName, "")), -1)
 
@@ -143,17 +143,17 @@ func (a *AccountApi) CreateAccountOld(ctx _context.Context, orgName string, clus
 	)
 	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
 	if err != nil {
-		return nil, err
+		return localVarReturnValue, nil, err
 	}
 
 	localVarHTTPResponse, err := a.Client.CallAPI(req)
 	if err != nil || localVarHTTPResponse == nil {
-		return localVarHTTPResponse, err
+		return localVarReturnValue, localVarHTTPResponse, err
 	}
 
 	localVarBody, err := common.ReadBody(localVarHTTPResponse)
 	if err != nil {
-		return localVarHTTPResponse, err
+		return localVarReturnValue, localVarHTTPResponse, err
 	}
 
 	if localVarHTTPResponse.StatusCode >= 300 {
@@ -161,18 +161,27 @@ func (a *AccountApi) CreateAccountOld(ctx _context.Context, orgName string, clus
 			ErrorBody:    localVarBody,
 			ErrorMessage: localVarHTTPResponse.Status,
 		}
-		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 || localVarHTTPResponse.StatusCode == 500 {
 			var v APIErrorResponse
 			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 			if err != nil {
-				return localVarHTTPResponse, newErr
+				return localVarReturnValue, localVarHTTPResponse, newErr
 			}
 			newErr.ErrorModel = v
 		}
-		return localVarHTTPResponse, newErr
+		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
 
-	return localVarHTTPResponse, nil
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
 // CreateMongoDBAccount Create mongodb account.
@@ -330,10 +339,9 @@ func (a *AccountApi) DeleteAccount(ctx _context.Context, engineName string, orgN
 	return localVarHTTPResponse, nil
 }
 
-// DeleteAccountOld Delete cluster account.
-// delete an account in cluster
-// Deprecated: This API is deprecated.
-func (a *AccountApi) DeleteAccountOld(ctx _context.Context, orgName string, clusterName string, accountName string) (*_nethttp.Response, error) {
+// DeleteHiveAccount Delete hive account.
+// delete an hive account in cluster
+func (a *AccountApi) DeleteHiveAccount(ctx _context.Context, orgName string, clusterName string, accountName string) (*_nethttp.Response, error) {
 	var (
 		localVarHTTPMethod = _nethttp.MethodDelete
 		localVarPostBody   interface{}
@@ -342,18 +350,18 @@ func (a *AccountApi) DeleteAccountOld(ctx _context.Context, orgName string, clus
 	// Add api info to context
 	apiInfo := common.APIInfo{
 		Tag:         "account",
-		OperationID: "deleteAccountOld",
-		Path:        "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}",
+		OperationID: "deleteHiveAccount",
+		Path:        "/api/v1/data/hive/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}",
 		Version:     "",
 	}
 	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
 
-	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.DeleteAccountOld")
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.DeleteHiveAccount")
 	if err != nil {
 		return nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
 	}
 
-	localVarPath := localBasePath + "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}"
+	localVarPath := localBasePath + "/api/v1/data/hive/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}"
 	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"clusterName"+"}", _neturl.PathEscape(common.ParameterToString(clusterName, "")), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"accountName"+"}", _neturl.PathEscape(common.ParameterToString(accountName, "")), -1)
@@ -388,7 +396,7 @@ func (a *AccountApi) DeleteAccountOld(ctx _context.Context, orgName string, clus
 			ErrorBody:    localVarBody,
 			ErrorMessage: localVarHTTPResponse.Status,
 		}
-		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 || localVarHTTPResponse.StatusCode == 500 {
 			var v APIErrorResponse
 			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 			if err != nil {
@@ -734,125 +742,6 @@ func (a *AccountApi) ListAccounts(ctx _context.Context, engineName string, orgNa
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
-// ListAccountsOldOptionalParameters holds optional parameters for ListAccountsOld.
-type ListAccountsOldOptionalParameters struct {
-	Component   *string
-	IncludeRoot *bool
-}
-
-// NewListAccountsOldOptionalParameters creates an empty struct for parameters.
-func NewListAccountsOldOptionalParameters() *ListAccountsOldOptionalParameters {
-	this := ListAccountsOldOptionalParameters{}
-	return &this
-}
-
-// WithComponent sets the corresponding parameter name and returns the struct.
-func (r *ListAccountsOldOptionalParameters) WithComponent(component string) *ListAccountsOldOptionalParameters {
-	r.Component = &component
-	return r
-}
-
-// WithIncludeRoot sets the corresponding parameter name and returns the struct.
-func (r *ListAccountsOldOptionalParameters) WithIncludeRoot(includeRoot bool) *ListAccountsOldOptionalParameters {
-	r.IncludeRoot = &includeRoot
-	return r
-}
-
-// ListAccountsOld List cluster accounts.
-// list accounts in cluster
-// Deprecated: This API is deprecated.
-func (a *AccountApi) ListAccountsOld(ctx _context.Context, orgName string, clusterName string, o ...ListAccountsOldOptionalParameters) ([]AccountListItem, *_nethttp.Response, error) {
-	var (
-		localVarHTTPMethod  = _nethttp.MethodGet
-		localVarPostBody    interface{}
-		localVarReturnValue []AccountListItem
-		optionalParams      ListAccountsOldOptionalParameters
-	)
-
-	if len(o) > 1 {
-		return localVarReturnValue, nil, common.ReportError("only one argument of type ListAccountsOldOptionalParameters is allowed")
-	}
-	if len(o) == 1 {
-		optionalParams = o[0]
-	}
-
-	// Add api info to context
-	apiInfo := common.APIInfo{
-		Tag:         "account",
-		OperationID: "listAccountsOld",
-		Path:        "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts",
-		Version:     "",
-	}
-	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
-
-	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.ListAccountsOld")
-	if err != nil {
-		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
-	}
-
-	localVarPath := localBasePath + "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts"
-	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
-	localVarPath = strings.Replace(localVarPath, "{"+"clusterName"+"}", _neturl.PathEscape(common.ParameterToString(clusterName, "")), -1)
-
-	localVarHeaderParams := make(map[string]string)
-	localVarQueryParams := _neturl.Values{}
-	localVarFormParams := _neturl.Values{}
-	if optionalParams.Component != nil {
-		localVarQueryParams.Add("component", common.ParameterToString(*optionalParams.Component, ""))
-	}
-	if optionalParams.IncludeRoot != nil {
-		localVarQueryParams.Add("includeRoot", common.ParameterToString(*optionalParams.IncludeRoot, ""))
-	}
-	localVarHeaderParams["Accept"] = "application/json"
-
-	common.SetAuthKeys(
-		ctx,
-		&localVarHeaderParams,
-		[2]string{"BearerToken", "authorization"},
-	)
-	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
-	if err != nil {
-		return localVarReturnValue, nil, err
-	}
-
-	localVarHTTPResponse, err := a.Client.CallAPI(req)
-	if err != nil || localVarHTTPResponse == nil {
-		return localVarReturnValue, localVarHTTPResponse, err
-	}
-
-	localVarBody, err := common.ReadBody(localVarHTTPResponse)
-	if err != nil {
-		return localVarReturnValue, localVarHTTPResponse, err
-	}
-
-	if localVarHTTPResponse.StatusCode >= 300 {
-		newErr := common.GenericOpenAPIError{
-			ErrorBody:    localVarBody,
-			ErrorMessage: localVarHTTPResponse.Status,
-		}
-		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
-			var v APIErrorResponse
-			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
-			if err != nil {
-				return localVarReturnValue, localVarHTTPResponse, newErr
-			}
-			newErr.ErrorModel = v
-		}
-		return localVarReturnValue, localVarHTTPResponse, newErr
-	}
-
-	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
-	if err != nil {
-		newErr := common.GenericOpenAPIError{
-			ErrorBody:    localVarBody,
-			ErrorMessage: err.Error(),
-		}
-		return localVarReturnValue, localVarHTTPResponse, newErr
-	}
-
-	return localVarReturnValue, localVarHTTPResponse, nil
-}
-
 // ListMongoDBAccounts List mongodb accounts.
 // list accounts in mongodb
 func (a *AccountApi) ListMongoDBAccounts(ctx _context.Context, orgName string, clusterName string) ([]AccountListItem, *_nethttp.Response, error) {
@@ -1008,81 +897,6 @@ func (a *AccountApi) UpdateAccount(ctx _context.Context, engineName string, orgN
 	return localVarHTTPResponse, nil
 }
 
-// UpdateAccountOld update cluster account.
-// update an account in cluster
-// Deprecated: This API is deprecated.
-func (a *AccountApi) UpdateAccountOld(ctx _context.Context, orgName string, clusterName string, accountName string, body Account) (*_nethttp.Response, error) {
-	var (
-		localVarHTTPMethod = _nethttp.MethodPatch
-		localVarPostBody   interface{}
-	)
-
-	// Add api info to context
-	apiInfo := common.APIInfo{
-		Tag:         "account",
-		OperationID: "updateAccountOld",
-		Path:        "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}",
-		Version:     "",
-	}
-	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
-
-	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.UpdateAccountOld")
-	if err != nil {
-		return nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
-	}
-
-	localVarPath := localBasePath + "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}"
-	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
-	localVarPath = strings.Replace(localVarPath, "{"+"clusterName"+"}", _neturl.PathEscape(common.ParameterToString(clusterName, "")), -1)
-	localVarPath = strings.Replace(localVarPath, "{"+"accountName"+"}", _neturl.PathEscape(common.ParameterToString(accountName, "")), -1)
-
-	localVarHeaderParams := make(map[string]string)
-	localVarQueryParams := _neturl.Values{}
-	localVarFormParams := _neturl.Values{}
-	localVarHeaderParams["Content-Type"] = "application/json"
-	localVarHeaderParams["Accept"] = "application/json"
-
-	// body params
-	localVarPostBody = &body
-	common.SetAuthKeys(
-		ctx,
-		&localVarHeaderParams,
-		[2]string{"BearerToken", "authorization"},
-	)
-	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	localVarHTTPResponse, err := a.Client.CallAPI(req)
-	if err != nil || localVarHTTPResponse == nil {
-		return localVarHTTPResponse, err
-	}
-
-	localVarBody, err := common.ReadBody(localVarHTTPResponse)
-	if err != nil {
-		return localVarHTTPResponse, err
-	}
-
-	if localVarHTTPResponse.StatusCode >= 300 {
-		newErr := common.GenericOpenAPIError{
-			ErrorBody:    localVarBody,
-			ErrorMessage: localVarHTTPResponse.Status,
-		}
-		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
-			var v APIErrorResponse
-			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
-			if err != nil {
-				return localVarHTTPResponse, newErr
-			}
-			newErr.ErrorModel = v
-		}
-		return localVarHTTPResponse, newErr
-	}
-
-	return localVarHTTPResponse, nil
-}
-
 // UpdateAccountPrivileges update account privileges.
 // update account privileges for rdbms engine
 func (a *AccountApi) UpdateAccountPrivileges(ctx _context.Context, engineName string, orgName string, clusterName string, accountName string, body []PrivilegeListItem) (*_nethttp.Response, error) {
@@ -1158,10 +972,9 @@ func (a *AccountApi) UpdateAccountPrivileges(ctx _context.Context, engineName st
 	return localVarHTTPResponse, nil
 }
 
-// UpdateAccountPrivilegesOld update account privileges.
-// update privileges of account
-// Deprecated: This API is deprecated.
-func (a *AccountApi) UpdateAccountPrivilegesOld(ctx _context.Context, orgName string, clusterName string, accountName string, body []PrivilegeListItem) (*_nethttp.Response, error) {
+// UpdateHiveAccount update hive account.
+// update an hive account in cluster
+func (a *AccountApi) UpdateHiveAccount(ctx _context.Context, orgName string, clusterName string, accountName string, body Account) (*_nethttp.Response, error) {
 	var (
 		localVarHTTPMethod = _nethttp.MethodPatch
 		localVarPostBody   interface{}
@@ -1170,18 +983,18 @@ func (a *AccountApi) UpdateAccountPrivilegesOld(ctx _context.Context, orgName st
 	// Add api info to context
 	apiInfo := common.APIInfo{
 		Tag:         "account",
-		OperationID: "updateAccountPrivilegesOld",
-		Path:        "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts/privileges/{accountName}",
+		OperationID: "updateHiveAccount",
+		Path:        "/api/v1/data/hive/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}",
 		Version:     "",
 	}
 	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
 
-	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.UpdateAccountPrivilegesOld")
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".AccountApi.UpdateHiveAccount")
 	if err != nil {
 		return nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
 	}
 
-	localVarPath := localBasePath + "/api/v1/organizations/{orgName}/clusters/{clusterName}/accounts/privileges/{accountName}"
+	localVarPath := localBasePath + "/api/v1/data/hive/organizations/{orgName}/clusters/{clusterName}/accounts/{accountName}"
 	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"clusterName"+"}", _neturl.PathEscape(common.ParameterToString(clusterName, "")), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"accountName"+"}", _neturl.PathEscape(common.ParameterToString(accountName, "")), -1)
@@ -1219,7 +1032,7 @@ func (a *AccountApi) UpdateAccountPrivilegesOld(ctx _context.Context, orgName st
 			ErrorBody:    localVarBody,
 			ErrorMessage: localVarHTTPResponse.Status,
 		}
-		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 || localVarHTTPResponse.StatusCode == 500 {
 			var v APIErrorResponse
 			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 			if err != nil {
