1aefbf4da662ffb2af0fe44da9f7521a19ac2eac
wangyelei <wangyelei@users.noreply.github.com>
2025-12-16 02:17:31 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/15344

--- api/kbcloud/admin/api_backup_repo.go
+++ api/kbcloud/admin/api_backup_repo.go
@@ -381,6 +381,86 @@ func (a *BackupRepoApi) GetBackupRepo(ctx _context.Context, environmentName stri
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+// ListBackupRepoFolder list backup repo folder.
+func (a *BackupRepoApi) ListBackupRepoFolder(ctx _context.Context, environmentName string, backupRepoName string, path string) (FileEntryList, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodPost
+		localVarPostBody    interface{}
+		localVarReturnValue FileEntryList
+	)
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "backupRepo",
+		OperationID: "listBackupRepoFolder",
+		Path:        "/admin/v1/environments/{environmentName}/backupRepo/{backupRepoName}/listFolder",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".BackupRepoApi.ListBackupRepoFolder")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/environments/{environmentName}/backupRepo/{backupRepoName}/listFolder"
+	localVarPath = strings.Replace(localVarPath, "{"+"environmentName"+"}", _neturl.PathEscape(common.ParameterToString(environmentName, "")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"backupRepoName"+"}", _neturl.PathEscape(common.ParameterToString(backupRepoName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarQueryParams.Add("path", common.ParameterToString(path, ""))
+	localVarHeaderParams["Accept"] = "application/json"
+
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"BearerToken", "authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 // ListBackupRepoStatsOptionalParameters holds optional parameters for ListBackupRepoStats.
 type ListBackupRepoStatsOptionalParameters struct {
 	Start *string
