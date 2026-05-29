6b986ffc9ca9d7fcbfce40ced11cdd15f9c988f0
1aal <1aal@users.noreply.github.com>
2026-05-29 03:47:30 +0000

    chore: auto update client api https://github.com/apecloud/apecloud/pull/18532

--- api/kbcloud/model_ai_agent_scope.go
+++ api/kbcloud/model_ai_agent_scope.go
@@ -7,12 +7,18 @@ package kbcloud
 import "github.com/apecloud/kb-cloud-client-go/api/common"
 
 type AiAgentScope struct {
-	OrgName         *string           `json:"orgName,omitempty"`
-	EnvironmentName *string           `json:"environmentName,omitempty"`
-	ClusterName     *string           `json:"clusterName,omitempty"`
-	Label           *string           `json:"label,omitempty"`
-	Namespace       *string           `json:"namespace,omitempty"`
-	ObjectRef       *AiAgentObjectRef `json:"objectRef,omitempty"`
+	OrgName             *string                  `json:"orgName,omitempty"`
+	EnvironmentName     *string                  `json:"environmentName,omitempty"`
+	ClusterName         *string                  `json:"clusterName,omitempty"`
+	KubernetesName      *string                  `json:"kubernetesName,omitempty"`
+	KubernetesNamespace *string                  `json:"kubernetesNamespace,omitempty"`
+	ClusterDefinition   *string                  `json:"clusterDefinition,omitempty"`
+	EngineMode          *string                  `json:"engineMode,omitempty"`
+	Components          []map[string]interface{} `json:"components,omitempty"`
+	ServiceRefs         []map[string]interface{} `json:"serviceRefs,omitempty"`
+	Label               *string                  `json:"label,omitempty"`
+	Namespace           *string                  `json:"namespace,omitempty"`
+	ObjectRef           *AiAgentObjectRef        `json:"objectRef,omitempty"`
 	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
 	UnparsedObject       map[string]interface{} `json:"-"`
 	AdditionalProperties map[string]interface{} `json:"-"`
@@ -119,6 +125,174 @@ func (o *AiAgentScope) SetClusterName(v string) {
 	o.ClusterName = &v
 }
 
+// GetKubernetesName returns the KubernetesName field value if set, zero value otherwise.
+func (o *AiAgentScope) GetKubernetesName() string {
+	if o == nil || o.KubernetesName == nil {
+		var ret string
+		return ret
+	}
+	return *o.KubernetesName
+}
+
+// GetKubernetesNameOk returns a tuple with the KubernetesName field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *AiAgentScope) GetKubernetesNameOk() (*string, bool) {
+	if o == nil || o.KubernetesName == nil {
+		return nil, false
+	}
+	return o.KubernetesName, true
+}
+
+// HasKubernetesName returns a boolean if a field has been set.
+func (o *AiAgentScope) HasKubernetesName() bool {
+	return o != nil && o.KubernetesName != nil
+}
+
+// SetKubernetesName gets a reference to the given string and assigns it to the KubernetesName field.
+func (o *AiAgentScope) SetKubernetesName(v string) {
+	o.KubernetesName = &v
+}
+
+// GetKubernetesNamespace returns the KubernetesNamespace field value if set, zero value otherwise.
+func (o *AiAgentScope) GetKubernetesNamespace() string {
+	if o == nil || o.KubernetesNamespace == nil {
+		var ret string
+		return ret
+	}
+	return *o.KubernetesNamespace
+}
+
+// GetKubernetesNamespaceOk returns a tuple with the KubernetesNamespace field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *AiAgentScope) GetKubernetesNamespaceOk() (*string, bool) {
+	if o == nil || o.KubernetesNamespace == nil {
+		return nil, false
+	}
+	return o.KubernetesNamespace, true
+}
+
+// HasKubernetesNamespace returns a boolean if a field has been set.
+func (o *AiAgentScope) HasKubernetesNamespace() bool {
+	return o != nil && o.KubernetesNamespace != nil
+}
+
+// SetKubernetesNamespace gets a reference to the given string and assigns it to the KubernetesNamespace field.
+func (o *AiAgentScope) SetKubernetesNamespace(v string) {
+	o.KubernetesNamespace = &v
+}
+
+// GetClusterDefinition returns the ClusterDefinition field value if set, zero value otherwise.
+func (o *AiAgentScope) GetClusterDefinition() string {
+	if o == nil || o.ClusterDefinition == nil {
+		var ret string
+		return ret
+	}
+	return *o.ClusterDefinition
+}
+
+// GetClusterDefinitionOk returns a tuple with the ClusterDefinition field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *AiAgentScope) GetClusterDefinitionOk() (*string, bool) {
+	if o == nil || o.ClusterDefinition == nil {
+		return nil, false
+	}
+	return o.ClusterDefinition, true
+}
+
+// HasClusterDefinition returns a boolean if a field has been set.
+func (o *AiAgentScope) HasClusterDefinition() bool {
+	return o != nil && o.ClusterDefinition != nil
+}
+
+// SetClusterDefinition gets a reference to the given string and assigns it to the ClusterDefinition field.
+func (o *AiAgentScope) SetClusterDefinition(v string) {
+	o.ClusterDefinition = &v
+}
+
+// GetEngineMode returns the EngineMode field value if set, zero value otherwise.
+func (o *AiAgentScope) GetEngineMode() string {
+	if o == nil || o.EngineMode == nil {
+		var ret string
+		return ret
+	}
+	return *o.EngineMode
+}
+
+// GetEngineModeOk returns a tuple with the EngineMode field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *AiAgentScope) GetEngineModeOk() (*string, bool) {
+	if o == nil || o.EngineMode == nil {
+		return nil, false
+	}
+	return o.EngineMode, true
+}
+
+// HasEngineMode returns a boolean if a field has been set.
+func (o *AiAgentScope) HasEngineMode() bool {
+	return o != nil && o.EngineMode != nil
+}
+
+// SetEngineMode gets a reference to the given string and assigns it to the EngineMode field.
+func (o *AiAgentScope) SetEngineMode(v string) {
+	o.EngineMode = &v
+}
+
+// GetComponents returns the Components field value if set, zero value otherwise.
+func (o *AiAgentScope) GetComponents() []map[string]interface{} {
+	if o == nil || o.Components == nil {
+		var ret []map[string]interface{}
+		return ret
+	}
+	return o.Components
+}
+
+// GetComponentsOk returns a tuple with the Components field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *AiAgentScope) GetComponentsOk() (*[]map[string]interface{}, bool) {
+	if o == nil || o.Components == nil {
+		return nil, false
+	}
+	return &o.Components, true
+}
+
+// HasComponents returns a boolean if a field has been set.
+func (o *AiAgentScope) HasComponents() bool {
+	return o != nil && o.Components != nil
+}
+
+// SetComponents gets a reference to the given []map[string]interface{} and assigns it to the Components field.
+func (o *AiAgentScope) SetComponents(v []map[string]interface{}) {
+	o.Components = v
+}
+
+// GetServiceRefs returns the ServiceRefs field value if set, zero value otherwise.
+func (o *AiAgentScope) GetServiceRefs() []map[string]interface{} {
+	if o == nil || o.ServiceRefs == nil {
+		var ret []map[string]interface{}
+		return ret
+	}
+	return o.ServiceRefs
+}
+
+// GetServiceRefsOk returns a tuple with the ServiceRefs field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *AiAgentScope) GetServiceRefsOk() (*[]map[string]interface{}, bool) {
+	if o == nil || o.ServiceRefs == nil {
+		return nil, false
+	}
+	return &o.ServiceRefs, true
+}
+
+// HasServiceRefs returns a boolean if a field has been set.
+func (o *AiAgentScope) HasServiceRefs() bool {
+	return o != nil && o.ServiceRefs != nil
+}
+
+// SetServiceRefs gets a reference to the given []map[string]interface{} and assigns it to the ServiceRefs field.
+func (o *AiAgentScope) SetServiceRefs(v []map[string]interface{}) {
+	o.ServiceRefs = v
+}
+
 // GetLabel returns the Label field value if set, zero value otherwise.
 func (o *AiAgentScope) GetLabel() string {
 	if o == nil || o.Label == nil {
@@ -218,6 +392,24 @@ func (o AiAgentScope) MarshalJSON() ([]byte, error) {
 	if o.ClusterName != nil {
 		toSerialize["clusterName"] = o.ClusterName
 	}
+	if o.KubernetesName != nil {
+		toSerialize["kubernetesName"] = o.KubernetesName
+	}
+	if o.KubernetesNamespace != nil {
+		toSerialize["kubernetesNamespace"] = o.KubernetesNamespace
+	}
+	if o.ClusterDefinition != nil {
+		toSerialize["clusterDefinition"] = o.ClusterDefinition
+	}
+	if o.EngineMode != nil {
+		toSerialize["engineMode"] = o.EngineMode
+	}
+	if o.Components != nil {
+		toSerialize["components"] = o.Components
+	}
+	if o.ServiceRefs != nil {
+		toSerialize["serviceRefs"] = o.ServiceRefs
+	}
 	if o.Label != nil {
 		toSerialize["label"] = o.Label
 	}
@@ -237,19 +429,25 @@ func (o AiAgentScope) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON deserializes the given payload.
 func (o *AiAgentScope) UnmarshalJSON(bytes []byte) (err error) {
 	all := struct {
-		OrgName         *string           `json:"orgName,omitempty"`
-		EnvironmentName *string           `json:"environmentName,omitempty"`
-		ClusterName     *string           `json:"clusterName,omitempty"`
-		Label           *string           `json:"label,omitempty"`
-		Namespace       *string           `json:"namespace,omitempty"`
-		ObjectRef       *AiAgentObjectRef `json:"objectRef,omitempty"`
+		OrgName             *string                  `json:"orgName,omitempty"`
+		EnvironmentName     *string                  `json:"environmentName,omitempty"`
+		ClusterName         *string                  `json:"clusterName,omitempty"`
+		KubernetesName      *string                  `json:"kubernetesName,omitempty"`
+		KubernetesNamespace *string                  `json:"kubernetesNamespace,omitempty"`
+		ClusterDefinition   *string                  `json:"clusterDefinition,omitempty"`
+		EngineMode          *string                  `json:"engineMode,omitempty"`
+		Components          []map[string]interface{} `json:"components,omitempty"`
+		ServiceRefs         []map[string]interface{} `json:"serviceRefs,omitempty"`
+		Label               *string                  `json:"label,omitempty"`
+		Namespace           *string                  `json:"namespace,omitempty"`
+		ObjectRef           *AiAgentObjectRef        `json:"objectRef,omitempty"`
 	}{}
 	if err = common.Unmarshal(bytes, &all); err != nil {
 		return err
 	}
 	additionalProperties := make(map[string]interface{})
 	if err = common.Unmarshal(bytes, &additionalProperties); err == nil {
-		common.DeleteKeys(additionalProperties, &[]string{"orgName", "environmentName", "clusterName", "label", "namespace", "objectRef"})
+		common.DeleteKeys(additionalProperties, &[]string{"orgName", "environmentName", "clusterName", "kubernetesName", "kubernetesNamespace", "clusterDefinition", "engineMode", "components", "serviceRefs", "label", "namespace", "objectRef"})
 	} else {
 		return err
 	}
@@ -258,6 +456,12 @@ func (o *AiAgentScope) UnmarshalJSON(bytes []byte) (err error) {
 	o.OrgName = all.OrgName
 	o.EnvironmentName = all.EnvironmentName
 	o.ClusterName = all.ClusterName
+	o.KubernetesName = all.KubernetesName
+	o.KubernetesNamespace = all.KubernetesNamespace
+	o.ClusterDefinition = all.ClusterDefinition
+	o.EngineMode = all.EngineMode
+	o.Components = all.Components
+	o.ServiceRefs = all.ServiceRefs
 	o.Label = all.Label
 	o.Namespace = all.Namespace
 	if all.ObjectRef != nil && all.ObjectRef.UnparsedObject != nil && o.UnparsedObject == nil {
