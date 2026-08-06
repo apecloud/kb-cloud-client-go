cd33861a3ffd45a8f15c7a8fcc02c37e5f5311ba
AurelianoLee <AurelianoLee@users.noreply.github.com>
2026-08-06 17:33:54 +0800

    chore: auto update client api https://github.com/apecloud/apecloud/pull/19979

--- api/kbcloud/admin/api_disaster_recovery.go
+++ api/kbcloud/admin/api_disaster_recovery.go
@@ -100,6 +100,283 @@ func (a *DisasterRecoveryApi) CreateDisasterRecovery(ctx _context.Context, paren
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+// DeleteDisasterRecoveryOptionalParameters holds optional parameters for DeleteDisasterRecovery.
+type DeleteDisasterRecoveryOptionalParameters struct {
+	LogicalInstanceId *string
+	ClusterId         *string
+}
+
+// NewDeleteDisasterRecoveryOptionalParameters creates an empty struct for parameters.
+func NewDeleteDisasterRecoveryOptionalParameters() *DeleteDisasterRecoveryOptionalParameters {
+	this := DeleteDisasterRecoveryOptionalParameters{}
+	return &this
+}
+
+// WithLogicalInstanceId sets the corresponding parameter name and returns the struct.
+func (r *DeleteDisasterRecoveryOptionalParameters) WithLogicalInstanceId(logicalInstanceId string) *DeleteDisasterRecoveryOptionalParameters {
+	r.LogicalInstanceId = &logicalInstanceId
+	return r
+}
+
+// WithClusterId sets the corresponding parameter name and returns the struct.
+func (r *DeleteDisasterRecoveryOptionalParameters) WithClusterId(clusterId string) *DeleteDisasterRecoveryOptionalParameters {
+	r.ClusterId = &clusterId
+	return r
+}
+
+// DeleteDisasterRecovery Delete a disaster recovery instance.
+// Delete a specific disaster recovery instance
+func (a *DisasterRecoveryApi) DeleteDisasterRecovery(ctx _context.Context, orgName string, o ...DeleteDisasterRecoveryOptionalParameters) (DisasterRecoveryTask, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodDelete
+		localVarPostBody    interface{}
+		localVarReturnValue DisasterRecoveryTask
+		optionalParams      DeleteDisasterRecoveryOptionalParameters
+	)
+
+	if len(o) > 1 {
+		return localVarReturnValue, nil, common.ReportError("only one argument of type DeleteDisasterRecoveryOptionalParameters is allowed")
+	}
+	if len(o) == 1 {
+		optionalParams = o[0]
+	}
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "disasterRecovery",
+		OperationID: "deleteDisasterRecovery",
+		Path:        "/admin/v1/organizations/{orgName}/disasterRecovery",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".DisasterRecoveryApi.DeleteDisasterRecovery")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/organizations/{orgName}/disasterRecovery"
+	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	if optionalParams.LogicalInstanceId != nil {
+		localVarQueryParams.Add("logicalInstanceID", common.ParameterToString(*optionalParams.LogicalInstanceId, ""))
+	}
+	if optionalParams.ClusterId != nil {
+		localVarQueryParams.Add("clusterID", common.ParameterToString(*optionalParams.ClusterId, ""))
+	}
+	localVarHeaderParams["Accept"] = "application/json"
+
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"DigestAuth", "Authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+// GetDisasterRecoveryHistory Get switch history of a disaster recovery instance.
+// Retrieve the history of failover and failback operations for a specific disaster recovery instance.
+func (a *DisasterRecoveryApi) GetDisasterRecoveryHistory(ctx _context.Context, clusterId string, orgName string) (DisasterRecoveryHistory, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodGet
+		localVarPostBody    interface{}
+		localVarReturnValue DisasterRecoveryHistory
+	)
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "disasterRecovery",
+		OperationID: "getDisasterRecoveryHistory",
+		Path:        "/admin/v1/organizations/{orgName}/disasterRecovery/{clusterID}/switchHistory",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".DisasterRecoveryApi.GetDisasterRecoveryHistory")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/organizations/{orgName}/disasterRecovery/{clusterID}/switchHistory"
+	localVarPath = strings.Replace(localVarPath, "{"+"clusterID"+"}", _neturl.PathEscape(common.ParameterToString(clusterId, "")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarHeaderParams["Accept"] = "application/json"
+
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"DigestAuth", "Authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+// GetDisasterRecoveryStatus Retrieve Disaster Recovery Instance Status.
+// Get detailed information about the status of a specific disaster recovery instance, including delay and current replication point.
+func (a *DisasterRecoveryApi) GetDisasterRecoveryStatus(ctx _context.Context, clusterId string, orgName string) (DisasterRecoveryStatusResponse, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodGet
+		localVarPostBody    interface{}
+		localVarReturnValue DisasterRecoveryStatusResponse
+	)
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "disasterRecovery",
+		OperationID: "getDisasterRecoveryStatus",
+		Path:        "/admin/v1/organizations/{orgName}/disasterRecovery/{clusterID}/status",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".DisasterRecoveryApi.GetDisasterRecoveryStatus")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/organizations/{orgName}/disasterRecovery/{clusterID}/status"
+	localVarPath = strings.Replace(localVarPath, "{"+"clusterID"+"}", _neturl.PathEscape(common.ParameterToString(clusterId, "")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarHeaderParams["Accept"] = "application/json"
+
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"DigestAuth", "Authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 // ListDisasterRecovery List Disaster Recovery instances under the main cluster.
 // Retrieve a list of disaster recovery instances for a specific database cluster.
 func (a *DisasterRecoveryApi) ListDisasterRecovery(ctx _context.Context, parentClusterId string, orgName string) (DisasterRecoveryClusterList, *_nethttp.Response, error) {
@@ -132,6 +409,89 @@ func (a *DisasterRecoveryApi) ListDisasterRecovery(ctx _context.Context, parentC
 	localVarFormParams := _neturl.Values{}
 	localVarHeaderParams["Accept"] = "application/json"
 
+	common.SetAuthKeys(
+		ctx,
+		&localVarHeaderParams,
+		[2]string{"DigestAuth", "Authorization"},
+	)
+	req, err := a.Client.PrepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, nil)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.Client.CallAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := common.ReadBody(localVarHTTPResponse)
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 || localVarHTTPResponse.StatusCode == 403 || localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = a.Client.Decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.ErrorModel = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := common.GenericOpenAPIError{
+			ErrorBody:    localVarBody,
+			ErrorMessage: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+// PromoteDisasterRecovery Promote a disaster recovery instance to the main instance.
+// Promote the disaster recovery instance to the primary database instance.
+func (a *DisasterRecoveryApi) PromoteDisasterRecovery(ctx _context.Context, clusterId string, orgName string, body DisasterRecoveryPromote) (DisasterRecoveryTask, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod  = _nethttp.MethodPost
+		localVarPostBody    interface{}
+		localVarReturnValue DisasterRecoveryTask
+	)
+
+	// Add api info to context
+	apiInfo := common.APIInfo{
+		Tag:         "disasterRecovery",
+		OperationID: "promoteDisasterRecovery",
+		Path:        "/admin/v1/organizations/{orgName}/disasterRecovery/{clusterID}/promote",
+		Version:     "",
+	}
+	ctx = context.WithValue(ctx, common.APIInfoCtxKey, apiInfo)
+
+	localBasePath, err := a.Client.Cfg.ServerURLWithContext(ctx, ".DisasterRecoveryApi.PromoteDisasterRecovery")
+	if err != nil {
+		return localVarReturnValue, nil, common.GenericOpenAPIError{ErrorMessage: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/admin/v1/organizations/{orgName}/disasterRecovery/{clusterID}/promote"
+	localVarPath = strings.Replace(localVarPath, "{"+"clusterID"+"}", _neturl.PathEscape(common.ParameterToString(clusterId, "")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"orgName"+"}", _neturl.PathEscape(common.ParameterToString(orgName, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	localVarHeaderParams["Content-Type"] = "application/json"
+	localVarHeaderParams["Accept"] = "application/json"
+
+	// body params
+	localVarPostBody = &body
 	common.SetAuthKeys(
 		ctx,
 		&localVarHeaderParams,
---
+++ api/kbcloud/model_kingbase_diagnostic_source.go
@@ -0,0 +1,205 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at ApeCloud (https://www.apecloud.com/).
+// Copyright 2022-Present ApeCloud Co., Ltd
+
+package kbcloud
+
+import (
+	"fmt"
+
+	"github.com/apecloud/kb-cloud-client-go/api/common"
+)
+
+type KingbaseDiagnosticSource struct {
+	Name        string                         `json:"name"`
+	Status      KingbaseDiagnosticSourceStatus `json:"status"`
+	CollectedAt string                         `json:"collectedAt"`
+	Message     *string                        `json:"message,omitempty"`
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject       map[string]interface{} `json:"-"`
+	AdditionalProperties map[string]interface{} `json:"-"`
+}
+
+// NewKingbaseDiagnosticSource instantiates a new KingbaseDiagnosticSource object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewKingbaseDiagnosticSource(name string, status KingbaseDiagnosticSourceStatus, collectedAt string) *KingbaseDiagnosticSource {
+	this := KingbaseDiagnosticSource{}
+	this.Name = name
+	this.Status = status
+	this.CollectedAt = collectedAt
+	return &this
+}
+
+// NewKingbaseDiagnosticSourceWithDefaults instantiates a new KingbaseDiagnosticSource object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewKingbaseDiagnosticSourceWithDefaults() *KingbaseDiagnosticSource {
+	this := KingbaseDiagnosticSource{}
+	return &this
+}
+
+// GetName returns the Name field value.
+func (o *KingbaseDiagnosticSource) GetName() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+	return o.Name
+}
+
+// GetNameOk returns a tuple with the Name field value
+// and a boolean to check if the value has been set.
+func (o *KingbaseDiagnosticSource) GetNameOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Name, true
+}
+
+// SetName sets field value.
+func (o *KingbaseDiagnosticSource) SetName(v string) {
+	o.Name = v
+}
+
+// GetStatus returns the Status field value.
+func (o *KingbaseDiagnosticSource) GetStatus() KingbaseDiagnosticSourceStatus {
+	if o == nil {
+		var ret KingbaseDiagnosticSourceStatus
+		return ret
+	}
+	return o.Status
+}
+
+// GetStatusOk returns a tuple with the Status field value
+// and a boolean to check if the value has been set.
+func (o *KingbaseDiagnosticSource) GetStatusOk() (*KingbaseDiagnosticSourceStatus, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Status, true
+}
+
+// SetStatus sets field value.
+func (o *KingbaseDiagnosticSource) SetStatus(v KingbaseDiagnosticSourceStatus) {
+	o.Status = v
+}
+
+// GetCollectedAt returns the CollectedAt field value.
+func (o *KingbaseDiagnosticSource) GetCollectedAt() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+	return o.CollectedAt
+}
+
+// GetCollectedAtOk returns a tuple with the CollectedAt field value
+// and a boolean to check if the value has been set.
+func (o *KingbaseDiagnosticSource) GetCollectedAtOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.CollectedAt, true
+}
+
+// SetCollectedAt sets field value.
+func (o *KingbaseDiagnosticSource) SetCollectedAt(v string) {
+	o.CollectedAt = v
+}
+
+// GetMessage returns the Message field value if set, zero value otherwise.
+func (o *KingbaseDiagnosticSource) GetMessage() string {
+	if o == nil || o.Message == nil {
+		var ret string
+		return ret
+	}
+	return *o.Message
+}
+
+// GetMessageOk returns a tuple with the Message field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *KingbaseDiagnosticSource) GetMessageOk() (*string, bool) {
+	if o == nil || o.Message == nil {
+		return nil, false
+	}
+	return o.Message, true
+}
+
+// HasMessage returns a boolean if a field has been set.
+func (o *KingbaseDiagnosticSource) HasMessage() bool {
+	return o != nil && o.Message != nil
+}
+
+// SetMessage gets a reference to the given string and assigns it to the Message field.
+func (o *KingbaseDiagnosticSource) SetMessage(v string) {
+	o.Message = &v
+}
+
+// MarshalJSON serializes the struct using spec logic.
+func (o KingbaseDiagnosticSource) MarshalJSON() ([]byte, error) {
+	toSerialize := map[string]interface{}{}
+	if o.UnparsedObject != nil {
+		return common.Marshal(o.UnparsedObject)
+	}
+	toSerialize["name"] = o.Name
+	toSerialize["status"] = o.Status
+	toSerialize["collectedAt"] = o.CollectedAt
+	if o.Message != nil {
+		toSerialize["message"] = o.Message
+	}
+
+	for key, value := range o.AdditionalProperties {
+		toSerialize[key] = value
+	}
+	return common.Marshal(toSerialize)
+}
+
+// UnmarshalJSON deserializes the given payload.
+func (o *KingbaseDiagnosticSource) UnmarshalJSON(bytes []byte) (err error) {
+	all := struct {
+		Name        *string                         `json:"name"`
+		Status      *KingbaseDiagnosticSourceStatus `json:"status"`
+		CollectedAt *string                         `json:"collectedAt"`
+		Message     *string                         `json:"message,omitempty"`
+	}{}
+	if err = common.Unmarshal(bytes, &all); err != nil {
+		return err
+	}
+	if all.Name == nil {
+		return fmt.Errorf("required field name missing")
+	}
+	if all.Status == nil {
+		return fmt.Errorf("required field status missing")
+	}
+	if all.CollectedAt == nil {
+		return fmt.Errorf("required field collectedAt missing")
+	}
+	additionalProperties := make(map[string]interface{})
+	if err = common.Unmarshal(bytes, &additionalProperties); err == nil {
+		common.DeleteKeys(additionalProperties, &[]string{"name", "status", "collectedAt", "message"})
+	} else {
+		return err
+	}
+
+	hasInvalidField := false
+	o.Name = *all.Name
+	if !all.Status.IsValid() {
+		hasInvalidField = true
+	} else {
+		o.Status = *all.Status
+	}
+	o.CollectedAt = *all.CollectedAt
+	o.Message = all.Message
+
+	if len(additionalProperties) > 0 {
+		o.AdditionalProperties = additionalProperties
+	}
+
+	if hasInvalidField {
+		return common.Unmarshal(bytes, &o.UnparsedObject)
+	}
+
+	return nil
+}
