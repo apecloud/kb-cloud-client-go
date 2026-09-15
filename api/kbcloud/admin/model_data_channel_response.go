e789f9d4f324c49f17b501de990396f72c10da43
caiq1nyu <caiq1nyu@users.noreply.github.com>
2026-09-15 17:19:56 +0800

    chore: auto update client api https://github.com/apecloud/apecloud/pull/20880

---
+++ api/kbcloud/admin/model_data_channel_response.go
@@ -0,0 +1,219 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at ApeCloud (https://www.apecloud.com/).
+// Copyright 2022-Present ApeCloud Co., Ltd
+
+package admin
+
+import (
+	"time"
+
+	"github.com/apecloud/kb-cloud-client-go/api/common"
+)
+
+type DataChannelResponse struct {
+	ChannelId   *string             `json:"channelID,omitempty"`
+	ChannelName *string             `json:"channelName,omitempty"`
+	CreatedAt   *time.Time          `json:"createdAt,omitempty"`
+	UpdatedAt   common.NullableTime `json:"updatedAt,omitempty"`
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject       map[string]interface{} `json:"-"`
+	AdditionalProperties map[string]interface{} `json:"-"`
+}
+
+// NewDataChannelResponse instantiates a new DataChannelResponse object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewDataChannelResponse() *DataChannelResponse {
+	this := DataChannelResponse{}
+	return &this
+}
+
+// NewDataChannelResponseWithDefaults instantiates a new DataChannelResponse object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewDataChannelResponseWithDefaults() *DataChannelResponse {
+	this := DataChannelResponse{}
+	return &this
+}
+
+// GetChannelId returns the ChannelId field value if set, zero value otherwise.
+func (o *DataChannelResponse) GetChannelId() string {
+	if o == nil || o.ChannelId == nil {
+		var ret string
+		return ret
+	}
+	return *o.ChannelId
+}
+
+// GetChannelIdOk returns a tuple with the ChannelId field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *DataChannelResponse) GetChannelIdOk() (*string, bool) {
+	if o == nil || o.ChannelId == nil {
+		return nil, false
+	}
+	return o.ChannelId, true
+}
+
+// HasChannelId returns a boolean if a field has been set.
+func (o *DataChannelResponse) HasChannelId() bool {
+	return o != nil && o.ChannelId != nil
+}
+
+// SetChannelId gets a reference to the given string and assigns it to the ChannelId field.
+func (o *DataChannelResponse) SetChannelId(v string) {
+	o.ChannelId = &v
+}
+
+// GetChannelName returns the ChannelName field value if set, zero value otherwise.
+func (o *DataChannelResponse) GetChannelName() string {
+	if o == nil || o.ChannelName == nil {
+		var ret string
+		return ret
+	}
+	return *o.ChannelName
+}
+
+// GetChannelNameOk returns a tuple with the ChannelName field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *DataChannelResponse) GetChannelNameOk() (*string, bool) {
+	if o == nil || o.ChannelName == nil {
+		return nil, false
+	}
+	return o.ChannelName, true
+}
+
+// HasChannelName returns a boolean if a field has been set.
+func (o *DataChannelResponse) HasChannelName() bool {
+	return o != nil && o.ChannelName != nil
+}
+
+// SetChannelName gets a reference to the given string and assigns it to the ChannelName field.
+func (o *DataChannelResponse) SetChannelName(v string) {
+	o.ChannelName = &v
+}
+
+// GetCreatedAt returns the CreatedAt field value if set, zero value otherwise.
+func (o *DataChannelResponse) GetCreatedAt() time.Time {
+	if o == nil || o.CreatedAt == nil {
+		var ret time.Time
+		return ret
+	}
+	return *o.CreatedAt
+}
+
+// GetCreatedAtOk returns a tuple with the CreatedAt field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *DataChannelResponse) GetCreatedAtOk() (*time.Time, bool) {
+	if o == nil || o.CreatedAt == nil {
+		return nil, false
+	}
+	return o.CreatedAt, true
+}
+
+// HasCreatedAt returns a boolean if a field has been set.
+func (o *DataChannelResponse) HasCreatedAt() bool {
+	return o != nil && o.CreatedAt != nil
+}
+
+// SetCreatedAt gets a reference to the given time.Time and assigns it to the CreatedAt field.
+func (o *DataChannelResponse) SetCreatedAt(v time.Time) {
+	o.CreatedAt = &v
+}
+
+// GetUpdatedAt returns the UpdatedAt field value if set, zero value otherwise (both if not set or set to explicit null).
+func (o *DataChannelResponse) GetUpdatedAt() time.Time {
+	if o == nil || o.UpdatedAt.Get() == nil {
+		var ret time.Time
+		return ret
+	}
+	return *o.UpdatedAt.Get()
+}
+
+// GetUpdatedAtOk returns a tuple with the UpdatedAt field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned.
+func (o *DataChannelResponse) GetUpdatedAtOk() (*time.Time, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return o.UpdatedAt.Get(), o.UpdatedAt.IsSet()
+}
+
+// HasUpdatedAt returns a boolean if a field has been set.
+func (o *DataChannelResponse) HasUpdatedAt() bool {
+	return o != nil && o.UpdatedAt.IsSet()
+}
+
+// SetUpdatedAt gets a reference to the given common.NullableTime and assigns it to the UpdatedAt field.
+func (o *DataChannelResponse) SetUpdatedAt(v time.Time) {
+	o.UpdatedAt.Set(&v)
+}
+
+// SetUpdatedAtNil sets the value for UpdatedAt to be an explicit nil.
+func (o *DataChannelResponse) SetUpdatedAtNil() {
+	o.UpdatedAt.Set(nil)
+}
+
+// UnsetUpdatedAt ensures that no value is present for UpdatedAt, not even an explicit nil.
+func (o *DataChannelResponse) UnsetUpdatedAt() {
+	o.UpdatedAt.Unset()
+}
+
+// MarshalJSON serializes the struct using spec logic.
+func (o DataChannelResponse) MarshalJSON() ([]byte, error) {
+	toSerialize := map[string]interface{}{}
+	if o.UnparsedObject != nil {
+		return common.Marshal(o.UnparsedObject)
+	}
+	if o.ChannelId != nil {
+		toSerialize["channelID"] = o.ChannelId
+	}
+	if o.ChannelName != nil {
+		toSerialize["channelName"] = o.ChannelName
+	}
+	if o.CreatedAt != nil {
+		if o.CreatedAt.Nanosecond() == 0 {
+			toSerialize["createdAt"] = o.CreatedAt.Format("2006-01-02T15:04:05Z07:00")
+		} else {
+			toSerialize["createdAt"] = o.CreatedAt.Format("2006-01-02T15:04:05.000Z07:00")
+		}
+	}
+	if o.UpdatedAt.IsSet() {
+		toSerialize["updatedAt"] = o.UpdatedAt.Get()
+	}
+
+	for key, value := range o.AdditionalProperties {
+		toSerialize[key] = value
+	}
+	return common.Marshal(toSerialize)
+}
+
+// UnmarshalJSON deserializes the given payload.
+func (o *DataChannelResponse) UnmarshalJSON(bytes []byte) (err error) {
+	all := struct {
+		ChannelId   *string             `json:"channelID,omitempty"`
+		ChannelName *string             `json:"channelName,omitempty"`
+		CreatedAt   *time.Time          `json:"createdAt,omitempty"`
+		UpdatedAt   common.NullableTime `json:"updatedAt,omitempty"`
+	}{}
+	if err = common.Unmarshal(bytes, &all); err != nil {
+		return err
+	}
+	additionalProperties := make(map[string]interface{})
+	if err = common.Unmarshal(bytes, &additionalProperties); err == nil {
+		common.DeleteKeys(additionalProperties, &[]string{"channelID", "channelName", "createdAt", "updatedAt"})
+	} else {
+		return err
+	}
+	o.ChannelId = all.ChannelId
+	o.ChannelName = all.ChannelName
+	o.CreatedAt = all.CreatedAt
+	o.UpdatedAt = all.UpdatedAt
+
+	if len(additionalProperties) > 0 {
+		o.AdditionalProperties = additionalProperties
+	}
+
+	return nil
+}
